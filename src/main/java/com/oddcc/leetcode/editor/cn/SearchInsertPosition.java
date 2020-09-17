3dc43b830179c3b4e40be999a86190c55391a1f9
oddcc <skycc71640@gmail.com>
2020-09-17 16:47:47 +0800

    search insert position 2

--- src/main/java/com/oddcc/leetcode/editor/cn/SearchInsertPosition.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/SearchInsertPosition.java
@@ -39,36 +39,30 @@ package com.oddcc.leetcode.editor.cn;
 public class SearchInsertPosition {
     public static void main(String[] args) {
         Solution solution = new SearchInsertPosition().new Solution();
-        System.out.println(solution.searchInsert(new int[]{1, 3, 5, 6}, 5));
-//        System.out.println(solution.searchInsert(new int[]{1,3,5,6}, 2));
-//        System.out.println(solution.searchInsert(new int[]{1,3,5,6}, 7));
-//        System.out.println(solution.searchInsert(new int[]{1,3,5,6,7}, 8));
-//        System.out.println(solution.searchInsert(new int[]{1}, 1));
+        System.out.println(solution.searchInsert(new int[]{1, 3, 5, 6}, 5)); // 2
+        System.out.println(solution.searchInsert(new int[]{1,3,5,6}, 2)); // 1
+        System.out.println(solution.searchInsert(new int[]{1,3,5,6}, 7)); // 4
+        System.out.println(solution.searchInsert(new int[]{1,3,5,6}, 0)); // 0
+        System.out.println(solution.searchInsert(new int[]{1,3,5,6,7}, 8)); // 5
+        System.out.println(solution.searchInsert(new int[]{1}, 1)); // 0
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int searchInsert(int[] nums, int target) {
-            if (nums.length == 0) return 0;
-            return search(nums, target, 0, nums.length - 1);
-        }
-
-        // 二分查找，如果找到就返回对应的位置，找不到就返回结束位置+1
-        private int search(int[] nums, int target, int start, int end) {
-            if (end <= start) {
-                if (nums[end] >= target) {
-                    return end;
+            int left = 0;
+            int right = nums.length - 1;
+            while (left < right) {
+                int mid = (right - left) / 2 + left;
+                if (nums[mid] == target) return mid;
+                if (nums[mid] < target) {
+                    left = mid + 1;
                 } else {
-                    return end + 1;
+                    right = mid;
                 }
             }
-            int mid = (end - start) / 2 + start;
-            if (nums[mid] == target) return mid;
-            if (target < nums[mid]) {
-                return search(nums, target, start, mid);
-            } else {
-                return search(nums, target, mid + 1, end);
-            }
+            if (nums[left] < target) left++;
+            return left;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
