01e5bbb8bd0f4e81ea467f6d3b5eeb9e6174d28c
oddcc <skycc71640@gmail.com>
2020-09-25 17:10:00 +0800

    110 2正确结果，但计算depth时，有不少重复计算

--- src/main/java/com/oddcc/leetcode/editor/cn/BalancedBinaryTree.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/BalancedBinaryTree.java
@@ -47,6 +47,7 @@ package com.oddcc.leetcode.editor.cn;
 import com.oddcc.leetcode.editor.cn.common.TreeNode;
 
 public class BalancedBinaryTree {
+    static int count = 0;
     public static void main(String[] args) {
         Solution solution = new BalancedBinaryTree().new Solution();
 //        TreeNode n1 = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
@@ -55,18 +56,22 @@ public class BalancedBinaryTree {
 //        System.out.println(solution.isBalanced(n2));
         TreeNode n3 = new TreeNode(1, new TreeNode(2, new TreeNode(3, new TreeNode(4), null), null), new TreeNode(2, null, new TreeNode(3, null, new TreeNode(4))));
         System.out.println(solution.isBalanced(n3));
+        System.out.println(count);
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 
     class Solution {
         public boolean isBalanced(TreeNode root) {
+            // a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+            // 定义很重要，不是一般意义上的平衡树，要求每个结点的左子树和右子树的高度差都不能超过1
             if (root == null) return true;
             int lDepth = getDepth(root.left);
             int rDepth = getDepth(root.right);
-            return Math.abs(lDepth - rDepth) <= 1;
+            return Math.abs(lDepth - rDepth) <= 1 && isBalanced(root.left) && isBalanced(root.right);
         }
 
         private int getDepth(TreeNode root) {
+            count++;
             if (root == null) return 0;
             return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
         }
