a83abab85c4809933de9b51c9fb689a6e2dab21d
oddcc <skycc71640@gmail.com>
2020-09-28 16:42:13 +0800

    141 Linked List Cycle 1 快慢指针

---
+++ src/main/java/com/oddcc/leetcode/editor/cn/common/ListNode.java
@@ -0,0 +1,16 @@
+package com.oddcc.leetcode.editor.cn.common;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
