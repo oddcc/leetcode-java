27536c5078825017a122376e9bc29015497e53fa
oddcc <skycc71640@gmail.com>
2020-10-02 00:09:14 +0800

    168 excel sheet comumn title

--- src/main/java/com/oddcc/leetcode/editor/cn/ExcelSheetColumnTitle.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/ExcelSheetColumnTitle.java
@@ -46,6 +46,7 @@ public class ExcelSheetColumnTitle {
     public static void main(String[] args) {
         Solution solution = new ExcelSheetColumnTitle().new Solution();
 //        System.out.println(solution.convertToTitle(701));
+//        System.out.println(solution.convertToTitle(702));
 //        System.out.println(solution.convertToTitle(1));
 //        System.out.println(solution.convertToTitle(28));
         System.out.println(solution.convertToTitle(52));
@@ -57,7 +58,8 @@ public class ExcelSheetColumnTitle {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public char[] chars = "0ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-        // 没有0，不是进制转换的问题
+
+        // 没有0，不是一般的进制转换问题
         public String convertToTitle(int n) {
             Deque<Integer> stack = new LinkedList<>();
             while (n > 26) {
@@ -68,8 +70,28 @@ public class ExcelSheetColumnTitle {
             }
             stack.push(n);
             StringBuilder sb = new StringBuilder();
+            boolean carry = stack.peekLast() == 0;
             while (!stack.isEmpty()) {
-                sb.append(chars[stack.pop()]);
+                int i = stack.pop();
+                if (stack.isEmpty() && i == 0 && carry) break;
+                if (i == 0) {
+                    sb.append(chars[26]);
+                    carry = true;
+                }
+                else {
+                    if (carry) {
+                        int i2 = i - 1;
+                        if (i2 == 0) {
+                            sb.append(chars[26]);
+                        }
+                        else {
+                            sb.append(chars[i2]);
+                            carry = false;
+                        }
+                    } else {
+                        sb.append(chars[i]);
+                    }
+                }
             }
             return sb.toString();
         }
