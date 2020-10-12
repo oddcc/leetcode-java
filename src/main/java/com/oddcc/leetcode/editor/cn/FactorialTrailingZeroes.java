52cd6db4e6e80300340d2f490c44b1817b205648
oddcc <skycc71640@gmail.com>
2020-10-12 15:41:08 +0800

    172 方法2通过5的出现次数计算有几个10，从而判断有几个0

--- src/main/java/com/oddcc/leetcode/editor/cn/FactorialTrailingZeroes.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/FactorialTrailingZeroes.java
@@ -1,20 +1,29 @@
-//package com.oddcc.leetcode.editor.cn;
-//
-//import java.math.BigInteger;
-//
-//public class FactorialTrailingZeroes {
-//    public static void main(String[] args) {
-//        Solution solution = new FactorialTrailingZeroes().new Solution();
-//        System.out.println(solution.trailingZeroes(30));
-//    }
-//
-//    //leetcode submit region begin(Prohibit modification and deletion)
-//    class Solution {
-//        // 方法1，先算出阶乘的结果，再看有几个零；n的范围太大，会溢出
-//        public int trailingZeroes(int n) {
-//
-//        }
-//    }
-////leetcode submit region end(Prohibit modification and deletion)
-//
-//}
+package com.oddcc.leetcode.editor.cn;
+
+public class FactorialTrailingZeroes {
+    public static void main(String[] args) {
+        Solution solution = new FactorialTrailingZeroes().new Solution();
+        System.out.println(solution.trailingZeroes(30));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int trailingZeroes(int n) {
+            int ans = 0;
+            for (int i = 1; i <= n; i++) {
+                int tmp = i;
+                while (tmp / 5 != 0) {
+                    if (tmp % 5 == 0) {
+                        ans++;
+                    } else {
+                        break;
+                    }
+                    tmp = tmp / 5;
+                }
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
