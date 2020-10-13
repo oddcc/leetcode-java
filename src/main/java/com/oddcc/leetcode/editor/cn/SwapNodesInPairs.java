db37f97a20cffb15b8dc53432feb8e16b0533084
oddcc <skycc71640@gmail.com>
2020-10-13 19:07:54 +0800

    24 每日打卡

---
+++ src/main/java/com/oddcc/leetcode/editor/cn/SwapNodesInPairs.java
@@ -0,0 +1,52 @@
+package com.oddcc.leetcode.editor.cn;
+
+import com.oddcc.leetcode.editor.cn.common.ListNode;
+
+public class SwapNodesInPairs {
+    public static void main(String[] args) {
+        Solution solution = new SwapNodesInPairs().new Solution();
+//        ListNode n1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+//        ListNode ans1 = solution.swapPairs(n1);
+//        System.out.println(ans1);
+//        ListNode n1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+//        ListNode ans1 = solution.swapPairs(n1);
+//        System.out.println(ans1);
+        ListNode n1 = ListNode.GetNodeList(2, 5, 3, 4, 6, 2, 2);
+        ListNode ans1 = solution.swapPairs(n1);
+        System.out.println(ans1);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        // 直接模拟，注意细节和边界条件
+        public ListNode swapPairs(ListNode head) {
+            if (head == null) return null;
+            if (head.next == null) return head;
+            ListNode dummyHead = null;
+            ListNode node = head;
+            ListNode pre = null;
+            ListNode post = null;
+            while (node != null && node.next != null) {
+                if (dummyHead == null) {
+                    dummyHead = node.next;
+                }
+                post = node.next.next;
+                node.next.next = node;
+                if (pre != null) {
+                    pre.next = node.next;
+                }
+                else {
+                    pre = dummyHead.next;
+                }
+                node.next = post;
+                if (pre != null) {
+                    pre = node;
+                }
+                node = node.next;
+            }
+            return dummyHead;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
--- src/main/java/com/oddcc/leetcode/editor/cn/common/ListNode.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/common/ListNode.java
@@ -13,4 +13,20 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    public static ListNode GetNodeList(Integer ...array) {
+        if (array.length == 0) return null;
+        ListNode ans = null;
+        ListNode head = ans;
+        for (Integer i: array) {
+            if (head == null) {
+                head = new ListNode(i);
+                ans = head;
+            } else {
+                head.next = new ListNode(i);
+                head = head.next;
+            }
+        }
+        return ans;
+    }
 }
