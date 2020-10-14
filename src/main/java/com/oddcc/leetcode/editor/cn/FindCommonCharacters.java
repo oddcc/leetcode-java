f6e545203f2ac1d3f5821f0d95b00651f8975f1d
oddcc <skycc71640@gmail.com>
2020-10-14 11:23:46 +0800

    1002 每日打卡，代码优化

--- src/main/java/com/oddcc/leetcode/editor/cn/FindCommonCharacters.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/FindCommonCharacters.java
@@ -1,9 +1,7 @@
 package com.oddcc.leetcode.editor.cn;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class FindCommonCharacters {
     public static void main(String[] args) {
@@ -16,38 +14,28 @@ public class FindCommonCharacters {
     class Solution {
         // 生成结果需要知道两个信息：1，字符是否在所有字符串中都出现过；2，在所有字符串中出现的最小次数是多少（不包括0）
         public List<String> commonChars(String[] A) {
-            // 0-出现在几个字符串中，1-截止目前，在单个字符串中出现的最小次数，2-在本字符串中出现几次
-            int[][] count = new int['z' - 'a' + 1][3];
-            for (int[] arr : count) {
-                Arrays.fill(arr, 0);
-            }
+            Map<Character, Integer> appearCount = new HashMap<>();
+            Map<Character, Integer> minAppearInOneString = new HashMap<>();
             for (int i = 0; i < A.length; i++) {
+                Map<Character, Integer> times = new HashMap<>();
                 for (char c : A[i].toCharArray()) {
-                    int charIndex = c - 'a';
-                    // 记录出现在几个字符串中
-                    if (count[charIndex][0] == i) {
-                        count[charIndex][0]++;
+                    int appear = appearCount.getOrDefault(c, 0);
+                    if (appear == i) {
+                        appearCount.put(c, appear + 1);
                     }
-                    count[charIndex][2]++;
+                    times.put(c, times.getOrDefault(c, 0) + 1);
                 }
-                // 记录单字符串中最小出现次数，本字符串中出现次数归零
-                for (int[] arr : count) {
-                    if (i == 0) {
-                        arr[1] = arr[2];
+                for (Map.Entry<Character, Integer> entry : times.entrySet()) {
+                    if (entry.getValue() < minAppearInOneString.getOrDefault(entry.getKey(), Integer.MAX_VALUE)) {
+                        minAppearInOneString.put(entry.getKey(), entry.getValue());
                     }
-                    if (arr[1] > 0) {
-                        if (arr[2] < arr[1]) {
-                            arr[1] = arr[2];
-                        }
-                    }
-                    arr[2] = 0;
                 }
             }
             List<String> ans = new ArrayList<>();
-            for (int i = 0; i < count.length; i++) {
-                if (count[i][0] == A.length) {
-                    for (int j = 0; j < count[i][1]; j++) {
-                        ans.add(String.valueOf((char) ('a' + i)));
+            for (Character c : minAppearInOneString.keySet()) {
+                if (appearCount.getOrDefault(c, 0) == A.length) {
+                    for (int i = 0; i < minAppearInOneString.getOrDefault(c, 0); i++) {
+                        ans.add(String.valueOf(c));
                     }
                 }
             }
