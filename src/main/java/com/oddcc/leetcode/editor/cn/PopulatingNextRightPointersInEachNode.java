0e4eeb12d0aa946d79b21fa5a92748cce9d08498
oddcc <skycc71640@gmail.com>
2020-10-15 14:34:41 +0800

    116 使用上层节点和下层节点的关系构建next关系

--- src/main/java/com/oddcc/leetcode/editor/cn/PopulatingNextRightPointersInEachNode.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/PopulatingNextRightPointersInEachNode.java
@@ -1,47 +1,36 @@
 package com.oddcc.leetcode.editor.cn;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class PopulatingNextRightPointersInEachNode {
     public static void main(String[] args) {
         Solution solution = new PopulatingNextRightPointersInEachNode().new Solution();
         Node n1 = new Node(1, new Node(2, new Node(4), new Node(5)), new Node(3, new Node(6), new Node(7)));
-        solution.bfs(n1);
+        solution.connect(n1);
         System.out.println(n1);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        // 使用层次遍历\BFS会有O(n)的空间复杂度，与题目要求的O(1)的空间复杂度不符
+        // 题目给出，这是一颗完美二叉树（perfect binary tree），每个非叶子节点必定有左子树和右子树
+        // 设当前节点为n的话，则有n.left.next = n.right，如果nn为n.next，则有n.right.next = nn.left
+        // 即下一层的next关系，可以通过上一层的节点来获得
         public Node connect(Node root) {
             if (root == null) return null;
-            bfs(root);
-            return root;
-        }
-
-        private void bfs(Node root) {
-            Queue<Node> queue = new LinkedList<>();
-            queue.offer(root);
-            while (!queue.isEmpty()) {
-                int len = queue.size();
-                Node pre = null;
-                for (int i = 0; i < len; i++) {
-                    Node node = queue.poll();
-                    if (pre == null) {
-                        pre = node;
-                    }
-                    else {
-                        pre.next = node;
-                        pre = node;
-                    }
-                    if (node.left != null) {
-                        queue.offer(node.left);
-                    }
-                    if (node.right != null) {
-                        queue.offer(node.right);
+            Node head = root;
+            while (head != null) {
+                Node cur = head;
+                while (cur != null) {
+                    if (cur.left == null) break;
+                    cur.left.next = cur.right;
+                    Node tmp = cur.right;
+                    cur = cur.next;
+                    if (cur != null) {
+                        tmp.next = cur.left;
                     }
                 }
+                head = head.left;
             }
+            return root;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
