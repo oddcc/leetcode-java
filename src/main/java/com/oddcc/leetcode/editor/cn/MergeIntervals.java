34642379583d3c053ce82596c08b0fa3e92d02ef
oddcc <skycc71640@gmail.com>
2020-10-16 16:27:04 +0800

    56 merge intervals 先排序，再合并，代码优化

--- src/main/java/com/oddcc/leetcode/editor/cn/MergeIntervals.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/MergeIntervals.java
@@ -14,17 +14,16 @@ public class MergeIntervals {
     class Solution {
         // 题目没提到区间是递增的，如果是递增的，就可以从头合并一遍，后面的区间合并，只要考虑前一个区间就可以
         public int[][] merge(int[][] intervals) {
-            List<List<Integer>> sorted = sort(intervals);
-            Deque<List<Integer>> ansStack = new LinkedList<>();
-            for (List<Integer> b : sorted) {
+            Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+            Deque<int[]> ansStack = new LinkedList<>();
+            for (int[] b : intervals) {
                 if (ansStack.isEmpty()) {
                     ansStack.push(b);
                     continue;
                 }
-                List<Integer> a = ansStack.pop();
+                int[] a = ansStack.pop();
                 if (overlap(a, b)) {
-                    List<Integer> merged = Arrays.asList(Math.min(a.get(0), b.get(0)), Math.max(a.get(1), b.get(1)));
-                    ansStack.push(merged);
+                    ansStack.push(new int[]{Math.min(a[0], b[0]), Math.max(a[1], b[1])});
                 }
                 else {
                     ansStack.push(a);
@@ -33,34 +32,21 @@ public class MergeIntervals {
             }
             int[][] des = new int[ansStack.size()][2];
             for (int i = 0; i < des.length; i++) {
-                List<Integer> t = ansStack.removeLast();
+                int[] t = ansStack.removeLast();
                 for (int j = 0; j < 2; j++) {
-                    des[i][j] = t.get(j);
+                    des[i][j] = t[j];
                 }
             }
             return des;
         }
 
-        private boolean overlap(List<Integer> a, List<Integer> b) {
+        private boolean overlap(int[] a, int[] b) {
             if (a == null || b == null) return false;
-            int aL = a.get(0);
-            int aH = a.get(1);
-            int bL = b.get(0);
-            int bH = b.get(1);
+            int aL = a[0];
+            int aH = a[1];
+            int bL = b[0];
+            int bH = b[1];
             return (bL >= aL && bL <= aH) || (bH >= aL && bH <= aH);
-        }
-
-        private List<List<Integer>> sort(int[][] src) {
-            List<List<Integer>> ans = new ArrayList<>(src.length);
-            for (int[] a : src) {
-                List<Integer> t = new ArrayList<>(2);
-                for (int i : a) {
-                    t.add(i);
-                }
-                ans.add(t);
-            }
-            ans.sort(Comparator.comparing(a -> a.get(0)));
-            return ans;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
