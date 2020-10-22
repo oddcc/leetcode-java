c4f04d91786a10950d97fc4d091833ae616a58e6
oddcc <skycc71640@gmail.com>
2020-10-22 10:52:22 +0800

    925 优化代码

--- src/main/java/com/oddcc/leetcode/editor/cn/LongPressedName.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/LongPressedName.java
@@ -3,45 +3,42 @@ package com.oddcc.leetcode.editor.cn;
 public class LongPressedName {
     public static void main(String[] args) {
         Solution solution = new LongPressedName().new Solution();
-//        System.out.println(solution.isLongPressedName("alex", "aaleex"));
-//        System.out.println(solution.isLongPressedName("saeed", "ssaaedd"));
-//        System.out.println(solution.isLongPressedName("leelee", "lleeelee"));
-//        System.out.println(solution.isLongPressedName("laiden", "laiden"));
-//        System.out.println(solution.isLongPressedName("vtkgn", "vttkgnn"));
-//        System.out.println(solution.isLongPressedName("pyplrz", "ppyypllr"));
-        System.out.println(solution.isLongPressedName("dfuyalc", "fuuyallc"));
+        System.out.println(solution.isLongPressedName("alex", "aaleex")); // T
+        System.out.println(solution.isLongPressedName("saeed", "ssaaedd"));// F
+        System.out.println(solution.isLongPressedName("leelee", "lleeelee"));// T
+        System.out.println(solution.isLongPressedName("laiden", "laiden"));// T
+        System.out.println(solution.isLongPressedName("vtkgn", "vttkgnn"));// T
+        System.out.println(solution.isLongPressedName("pyplrz", "ppyypllr"));// F
+        System.out.println(solution.isLongPressedName("dfuyalc", "fuuyallc"));// F
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         // 两个指针，一次遍历
         public boolean isLongPressedName(String name, String typed) {
-            int nameIndex = 0;
-            Character tmpC = null;
-            // 如果跟name相同，则nameIndex++，tmp设为当前字符，如果不同，但是跟tmp相同，则nameIndex不变，如果不同，且跟tmp不同，则返回false
-            for (int i = 0; i < typed.length(); i++) {
-                char typedChar = typed.charAt(i);
-                if (nameIndex > name.length() - 1) {
-                    if (tmpC == null) return false;
-                    if (tmpC != typedChar) {
-                        return false;
-                    }
-                    else {
-                        continue;
-                    }
-                }
-                char nameChar = name.charAt(nameIndex);
-                if (nameChar != typedChar) {
-                    if (tmpC == null) return false;
-                    if (typedChar != tmpC) return false;
+            int pName = 0;
+            int pTyped = 0;
+            Character lastChar = null;
+            while (pName < name.length() || pTyped < typed.length()) {
+                if (pName >= name.length()) pName = name.length() - 1; // 这种情况，会出现在 vtkgn vttkgnn 满足条件之后 typed 都是重复的，会继续往后走
+                if (pTyped >= typed.length()) return false; // 这种情况，会出现在 pyplrz ppyypllrrrrrr，name一直没满足，typed一直往后走
+                char nameChar = name.charAt(pName);
+                char typedChar = typed.charAt(pTyped);
+                if (nameChar == typedChar) {
+                    lastChar = typedChar;
+                    pName++;
+                    pTyped++;
                 }
                 else {
-                    nameIndex++;
-                    tmpC = nameChar;
+                    if (lastChar != null && typedChar == lastChar) {
+                        pTyped++;
+                    }
+                    else {
+                        return false;
+                    }
                 }
             }
-            if (name.charAt(name.length() - 1) != typed.charAt(typed.length() - 1)) return false;
-            return true;
+            return pName == name.length() && pTyped == typed.length();
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ src/main/java/com/oddcc/leetcode/editor/cn/PartitionLabels.java
@@ -0,0 +1,16 @@
+//package com.oddcc.leetcode.editor.cn;
+//
+//public class PartitionLabels {
+//    public static void main(String[] args) {
+//        Solution solution = new PartitionLabels().new Solution();
+//    }
+//
+//    //leetcode submit region begin(Prohibit modification and deletion)
+//    class Solution {
+//        public List<Integer> partitionLabels(String S) {
+//
+//        }
+//    }
+////leetcode submit region end(Prohibit modification and deletion)
+//
+//}
