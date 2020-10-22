aa1b970938e02a64fc4ab0970cad673e9e1f8ef8
oddcc <skycc71640@gmail.com>
2020-10-22 11:47:41 +0800

    763 每日打卡

--- src/main/java/com/oddcc/leetcode/editor/cn/PartitionLabels.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/PartitionLabels.java
@@ -1,16 +1,40 @@
-//package com.oddcc.leetcode.editor.cn;
-//
-//public class PartitionLabels {
-//    public static void main(String[] args) {
-//        Solution solution = new PartitionLabels().new Solution();
-//    }
-//
-//    //leetcode submit region begin(Prohibit modification and deletion)
-//    class Solution {
-//        public List<Integer> partitionLabels(String S) {
-//
-//        }
-//    }
-////leetcode submit region end(Prohibit modification and deletion)
-//
-//}
+package com.oddcc.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PartitionLabels {
+    public static void main(String[] args) {
+        Solution solution = new PartitionLabels().new Solution();
+        System.out.println(solution.partitionLabels("ababcbacadefegdehijhklij"));
+        System.out.println(solution.partitionLabels("caedbdedda"));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        // 思路是找到当前字母开始出现的位置start和最后出现的位置end，分段至少要包含 start和end
+        // 从start开始向后遍历，每次遇到一个字母，如果end大于已有的end，就要扩充end的范围，直到和end相遇，说明访问过的所有字母，都在start～end的范围内了，这样才能确定一个分段
+        public List<Integer> partitionLabels(String S) {
+            List<Integer> ans = new ArrayList<>();
+            int start = 0;
+            int end = 0;
+            int current = 0;
+            while (end <= S.length() - 1 && current <= S.length() - 1) {
+                char c = S.charAt(current);
+                end = Math.max(end, S.lastIndexOf(c));
+                if (current == end) {
+                    ans.add(end - start + 1);
+                    start = end + 1;
+                    current = end + 1;
+                    end = end + 1;
+                }
+                else {
+                    current++;
+                }
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
