347ddebbdac46d8af528f9a66c144afa111e543c
oddcc <skycc71640@gmail.com>
2020-10-23 10:56:59 +0800

    234 每日打卡，转化为数组

---
+++ src/main/java/com/oddcc/leetcode/editor/cn/PalindromeLinkedList.java
@@ -0,0 +1,43 @@
+package com.oddcc.leetcode.editor.cn;
+
+import com.oddcc.leetcode.editor.cn.common.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromeLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new PalindromeLinkedList().new Solution();
+        System.out.println(solution.isPalindrome(ListNode.GetNodeList(1,2)));
+        System.out.println(solution.isPalindrome(ListNode.GetNodeList(1,2,2,1)));
+        System.out.println(solution.isPalindrome(ListNode.GetNodeList(1)));
+        System.out.println(solution.isPalindrome(ListNode.GetNodeList(1,1)));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        // 思路1，先存下来位置，然后再从两边遍历，时间复杂度O(n)，空间复杂度O(n)
+        public boolean isPalindrome(ListNode head) {
+            if (head == null) return true;
+            List<Integer> cache = new ArrayList<>();
+            while (head != null) {
+                cache.add(head.val);
+                head = head.next;
+            }
+            int left = 0;
+            int right = cache.size() - 1;
+            while (left < right) {
+                if (cache.get(left).equals(cache.get(right))) {
+                    left++;
+                    right--;
+                }
+                else {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
