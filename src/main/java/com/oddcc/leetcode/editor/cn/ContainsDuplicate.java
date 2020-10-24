d9b96403f71ec1d9b49220977f0ac718ca051f97
oddcc <skycc71640@gmail.com>
2020-10-24 23:16:35 +0800

    217 思路2，先排序

--- src/main/java/com/oddcc/leetcode/editor/cn/ContainsDuplicate.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/ContainsDuplicate.java
@@ -1,5 +1,6 @@
 package com.oddcc.leetcode.editor.cn;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,14 +11,21 @@ public class ContainsDuplicate {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        // 思路1，记录访问过的数，哈希表
+        // 思路1，记录访问过的数，哈希表；时间复杂度O(n)，空间复杂度O(n)
+        // 思路2，先排序，然后从头遍历，时间复杂度O(nlogn)，空间复杂度取决于排序算法的实现
         public boolean containsDuplicate(int[] nums) {
-            Set<Integer> cache = new HashSet<>();
-            for (int i: nums) {
-                if (cache.contains(i)) {
-                    return true;
-                } else {
-                    cache.add(i);
+            Arrays.sort(nums);
+            Integer tmp = null;
+            for (int i : nums) {
+                if (tmp == null) {
+                    tmp = i;
+                    continue;
+                }
+                else {
+                    if (tmp == i) {
+                        return true;
+                    }
+                    tmp = i;
                 }
             }
             return false;
