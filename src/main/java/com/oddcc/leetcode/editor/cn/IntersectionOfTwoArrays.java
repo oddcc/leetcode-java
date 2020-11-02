97b42fcf373870a8731f5ac9525aa4d7a79bbf2e
oddcc <skycc71640@gmail.com>
2020-11-02 10:20:41 +0800

    349 每日打卡 利用哈希表

---
+++ src/main/java/com/oddcc/leetcode/editor/cn/IntersectionOfTwoArrays.java
@@ -0,0 +1,38 @@
+package com.oddcc.leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionOfTwoArrays {
+    public static void main(String[] args) {
+        Solution solution = new IntersectionOfTwoArrays().new Solution();
+//        System.out.println(Arrays.toString(solution.intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2})));
+        System.out.println(Arrays.toString(solution.intersection(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4})));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        // 求交集，array中可能有重复元素
+        // 思路1，哈希表，先遍历nums1，再遍历nums2，如果遍历nums2时遇到哈希表中存在的元素，则加入结果集；时间复杂度O(m + n)，空间复杂度O(max(m, n))
+        // 思路2，先排序，然后同时遍历nums1和nums2，如果遇到相同的，就加入结果集，移动两个指针，否则移动小的那边的指针
+        public int[] intersection(int[] nums1, int[] nums2) {
+            Set<Integer> cache = new HashSet<>();
+            Set<Integer> ansSet = new HashSet<>();
+            for (int i : nums1) {
+                cache.add(i);
+            }
+            for (int i : nums2) {
+                if (cache.contains(i)) ansSet.add(i);
+            }
+            int[] ans = new int[ansSet.size()];
+            int i = 0;
+            for (int num : ansSet) {
+                ans[i++] = num;
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
