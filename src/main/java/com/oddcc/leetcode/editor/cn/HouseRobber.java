16e78401010601f564c25323a9b680a81805b81a
oddcc <skycc71640@gmail.com>
2020-11-02 11:31:00 +0800

    198 动态规划

---
+++ src/main/java/com/oddcc/leetcode/editor/cn/HouseRobber.java
@@ -0,0 +1,31 @@
+package com.oddcc.leetcode.editor.cn;
+
+public class HouseRobber {
+    public static void main(String[] args) {
+        Solution solution = new HouseRobber().new Solution();
+        System.out.println(solution.rob(new int[]{1, 2, 3, 1}));
+        System.out.println(solution.rob(new int[]{2, 7, 9, 3, 1}));
+        System.out.println(solution.rob(new int[]{1}));
+        System.out.println(solution.rob(new int[]{0}));
+        System.out.println(solution.rob(new int[]{2, 1, 1, 2}));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int rob(int[] nums) {
+            if (nums.length == 0) return 0;
+            if (nums.length == 1) return nums[0];
+            // dp[i]表示从前往后选，截止到i时的最大加和
+            int[] dp = new int[nums.length];
+            dp[0] = nums[0];
+            dp[1] = Math.max(nums[1], nums[0]);
+            for (int i = 2; i < nums.length; i++) {
+                //               选了i的结果          没选i的结果
+                dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+            }
+            return dp[nums.length - 1];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
