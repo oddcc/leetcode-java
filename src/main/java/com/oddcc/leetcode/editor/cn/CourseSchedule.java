2ccc7464daa848b89e95cf232b4542b486d7436f
oddcc <skycc71640@gmail.com>
2020-11-04 16:40:04 +0800

    152 动态规划法，代码优化

---
+++ src/main/java/com/oddcc/leetcode/editor/cn/CourseSchedule.java
@@ -0,0 +1,16 @@
+//package com.oddcc.leetcode.editor.cn;
+//
+//public class CourseSchedule {
+//    public static void main(String[] args) {
+//        Solution solution = new CourseSchedule().new Solution();
+//    }
+//
+//    //leetcode submit region begin(Prohibit modification and deletion)
+//    class Solution {
+//        public boolean canFinish(int numCourses, int[][] prerequisites) {
+//
+//        }
+//    }
+////leetcode submit region end(Prohibit modification and deletion)
+//
+//}
--- src/main/java/com/oddcc/leetcode/editor/cn/MaximumSubarray.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/MaximumSubarray.java
@@ -74,14 +74,7 @@ public class MaximumSubarray {
             int[] dp = new int[len];
             dp[0] = nums[0];
             for (int i = 1; i < len; i++) {
-                int sum;
-                if (dp[i] == 0) {
-                    sum = Math.max(dp[i - 1] + nums[i], nums[i]);
-                    dp[i] = sum;
-                }
-                else {
-                    sum = dp[i];
-                }
+                dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
             }
             int max = dp[0];
             for (int i : dp) {
