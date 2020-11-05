d930d7796bba5d73331335e12f9bf59b9912c2fc
oddcc <skycc71640@gmail.com>
2020-11-05 14:45:46 +0800

    127 每日打卡，bfs

--- src/main/java/com/oddcc/leetcode/editor/cn/WordLadder.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/WordLadder.java
@@ -10,59 +10,39 @@ public class WordLadder {
         System.out.println(solution.ladderLength("a", "c", Arrays.asList("a", "b", "c")));
         System.out.println(solution.ladderLength("hot", "dog", Arrays.asList("hot", "dog")));
         System.out.println(solution.ladderLength("hot", "dog", Arrays.asList("hot", "cog", "dog", "tot", "hog", "hop", "pot", "dot")));
+        System.out.println(solution.ladderLength("cet", "ism", Arrays.asList("kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob")));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        // 由题目要求最短路径想到BFS
-        // 选跟start相差1个字符的进行搜索，选中的字符作为下一次搜索的起点，直到start跟end只相差1个字符
-        // 搜索过程中也记录可能中是否有end，如果没有的话，可以提前return
         public int ladderLength(String beginWord, String endWord, List<String> wordList) {
             if (!wordList.contains(endWord)) return 0;
             int ans = 0;
             Queue<String> beginWords = new LinkedList<>();
-            Queue<List<String>> optionWords = new LinkedList<>();
+            Set<String> visited = new HashSet<>();
             beginWords.add(beginWord);
-            optionWords.add(wordList);
-            // beginWords还有，说明还可以继续搜索
+            visited.add(beginWord);
             while (!beginWords.isEmpty()) {
-//                System.out.println("************");
-                // 用于存放下一层的beginWords和optionWords
-                Queue<String> bList = new LinkedList<>();
-                Queue<List<String>> oList = new LinkedList<>();
-//                System.out.println(beginWords);
-//                System.out.println(optionWords);
-//                System.out.println("=====");
-                while (!beginWords.isEmpty()) {
+                ans++;
+                int len = beginWords.size();
+                for (int i = 0; i < len; i++) {
                     String begin = beginWords.remove();
-                    List<String> options = optionWords.remove();
-                    // 如果begin和end只相差一位，说明找到结果了，直接返回，要把这一层加上去
-                    if (differentCount(begin, endWord) == 1) {
-                        ans++;
-                        return ans + 1;
+                    if (begin.equals(endWord)) {
+                        return ans;
                     }
-                    // 遍历每个选项，如果有跟begin差一位的，说明还能继续进行搜索，放入下一层的搜索集
-                    for (String w : options) {
-                        if (differentCount(w, begin) == 1) {
-                            bList.add(w);
-                            oList.add(getOptions(w, options));
+                    for (String s: wordList) {
+                        if (visited.contains(s)) continue;
+                        if (differentCount(begin, s) == 1) {
+                            visited.add(s);
+                            beginWords.add(s);
                         }
                     }
                 }
-                ans++;
-                beginWords = bList;
-                optionWords = oList;
             }
             return 0; // 如果有结果，总应该出现在上方循环内
         }
 
-        private List<String> getOptions(String word, List<String> wordList) {
-            List<String> copy = new ArrayList<>(wordList);
-            copy.remove(word);
-            return copy;
-        }
-
-        // w1和w2的长度总是相同
+        // 题设w1和w2的长度总是相同
         private int differentCount(String w1, String w2) {
             if (w1.equals(w2)) return 0;
             int count = 0;
