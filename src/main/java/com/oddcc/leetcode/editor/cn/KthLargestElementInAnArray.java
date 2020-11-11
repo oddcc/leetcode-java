88ddde12a489a7d21f45ade88a3d7c97a07fc31c
oddcc <skycc71640@gmail.com>
2020-11-11 16:14:35 +0800

    215 第K大，维护K个元素的最小堆，更大的元素入堆，遍历完成后，堆顶元素就是第K大的元素

--- src/main/java/com/oddcc/leetcode/editor/cn/KthLargestElementInAnArray.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/KthLargestElementInAnArray.java
@@ -16,18 +16,18 @@ public class KthLargestElementInAnArray {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         // 思路1，第K大的元素，第一想法是使用堆；有len个元素，第K大，全部入堆，再弹出K-1个，堆顶就是第K大
+        // 思路2，维护K个元素的最小堆，更大的元素入堆，遍历完成后，堆顶元素就是第K大的元素
         public int findKthLargest(int[] nums, int k) {
-            PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    return o2 - o1;
+            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+            for (int i = 0; i < k; i++) minHeap.add(nums[i]);
+            for (int i = k; i < nums.length; i++) {
+                int top = minHeap.peek();
+                if (nums[i] > top) {
+                    minHeap.poll();
+                    minHeap.add(nums[i]);
                 }
-            });
-            for (int i : nums) {
-                maxHeap.add(i);
             }
-            for (int i = 0; i < k - 1; i++) maxHeap.poll();
-            return maxHeap.poll();
+            return minHeap.poll();
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
