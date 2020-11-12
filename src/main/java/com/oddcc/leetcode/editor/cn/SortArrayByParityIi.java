4f3e788ace65c6597af18cb8c95147db7c1f6460
oddcc <skycc71640@gmail.com>
2020-11-12 10:59:38 +0800

    922 每日打卡，奇数指针和偶数指针

--- src/main/java/com/oddcc/leetcode/editor/cn/SortArrayByParityIi.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/SortArrayByParityIi.java
@@ -16,26 +16,18 @@ public class SortArrayByParityIi {
         // 思路1，双指针，从左遍历，如果碰到不符合的（比如A[i]是偶数，i是奇数或A[i]是奇数，i是偶数），就往后找一个不符合的，并交换，因为奇数偶数是一半一半，所以一次交换会减少两个不符合的情况
         // 思路2，基于思路1，每有一个错位的奇数，就有一个错位的偶数对应，可以考虑双指针从两边同时遍历；由于分奇偶两种情况，可以考虑遍历两次
         // 思路3，基于思路1优化，当找对应位置的时候，因为只要找奇数或偶数位置，每次指针移动可以移动2位
+        // 思路4，奇数指针和偶数指针
         public int[] sortArrayByParityII(int[] A) {
-            for (int i = 0; i < A.length; i++) {
-                // 找奇数
-                if (isEven(A[i]) && isOdd(i)) {
-                    for (int j = i + 1; j < A.length; j += 2) {
-                        if (isOdd(A[j]) && isEven(j)) {
-                            swap(A, i, j);
-                            break;
-                        }
-                    }
+            int even = 0;
+            int odd = 1;
+            while (even < A.length && odd < A.length) {
+                while (even < A.length && !isOdd(A[even])) {
+                    even += 2;
                 }
-                // 找偶数
-                if (isOdd(A[i]) && isEven(i)) {
-                    for (int j = i + 1; j < A.length; j += 2) {
-                        if (isEven(A[j]) && isOdd(j)) {
-                            swap(A, i, j);
-                            break;
-                        }
-                    }
+                while (odd < A.length && !isEven(A[odd])) {
+                    odd += 2;
                 }
+                if (odd < A.length && even < A.length) swap(A, odd, even);
             }
             return A;
         }
