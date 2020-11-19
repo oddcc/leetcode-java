9ce61532a187c7c7447dd61501b1e24ab989a0d5
oddcc <skycc71640@gmail.com>
2020-11-19 14:39:47 +0800

    283 每日打卡，代码优化

--- src/main/java/com/oddcc/leetcode/editor/cn/MoveZeroes.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/MoveZeroes.java
@@ -8,28 +8,33 @@ public class MoveZeroes {
     public static void main(String[] args) {
         Solution solution = new MoveZeroes().new Solution();
         int[] n1 = new int[]{0, 1, 0, 3, 12};
-        // int[] n1 = new int[]{};
+        // int[] n1 = new int[]{1, 0};
         solution.moveZeroes(n1);
         System.out.println(Arrays.toString(n1));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        // 题目要求in-place操作，想到双指针a和b，其中a指向0，b指向非0；a从前往后遍历，b每次从a的位置向后找，找到就交换，当a到达末尾时流程结束
+        // 思路1，题目要求in-place操作，想到双指针a和b，其中a指向0，b指向非0；a从前往后遍历，b每次从a的位置向后找，找到就交换，当a到达末尾时流程结束
+        // 时间复杂度可能达到O(N^2)
+        // 思路2，每次交换之后，b指针不用从a的地方从新开始，因为b之前都是0，不然b就不会指向当前的位置
         public void moveZeroes(int[] nums) {
-            for (int zero = 0; zero < nums.length; zero++) {
-                if (nums[zero] == 0) {
-                    int nonZero = zero + 1;
-                    while (nonZero < nums.length) {
-                        if (nums[nonZero] != 0) {
-                            swap(nums, zero, nonZero);
+            int left = 0;
+            int right = 0;
+            while (left < nums.length) {
+                if (nums[left] == 0) {
+                    while (right < nums.length) {
+                        // 只有right在left右边时才交换
+                        if (nums[right] != 0 && left < right) {
+                            swap(nums, left, right);
                             break;
                         }
                         else {
-                            nonZero++;
+                            right++;
                         }
                     }
                 }
+                left++;
             }
         }
 
