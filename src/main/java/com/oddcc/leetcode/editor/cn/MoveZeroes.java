821a083d5cc11a916d6de31994a1d67abd25a366
oddcc <skycc71640@gmail.com>
2020-11-19 14:42:01 +0800

    283 每日打卡，代码优化2

--- src/main/java/com/oddcc/leetcode/editor/cn/MoveZeroes.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/MoveZeroes.java
@@ -18,23 +18,16 @@ public class MoveZeroes {
         // 思路1，题目要求in-place操作，想到双指针a和b，其中a指向0，b指向非0；a从前往后遍历，b每次从a的位置向后找，找到就交换，当a到达末尾时流程结束
         // 时间复杂度可能达到O(N^2)
         // 思路2，每次交换之后，b指针不用从a的地方从新开始，因为b之前都是0，不然b就不会指向当前的位置
+        // 思路3，碰到非0就往前排，直到遍历完成
         public void moveZeroes(int[] nums) {
             int left = 0;
             int right = 0;
-            while (left < nums.length) {
-                if (nums[left] == 0) {
-                    while (right < nums.length) {
-                        // 只有right在left右边时才交换
-                        if (nums[right] != 0 && left < right) {
-                            swap(nums, left, right);
-                            break;
-                        }
-                        else {
-                            right++;
-                        }
-                    }
+            while (right < nums.length) {
+                if (nums[right] != 0) {
+                    swap(nums, left, right);
+                    left++;
                 }
-                left++;
+                right++;
             }
         }
 
