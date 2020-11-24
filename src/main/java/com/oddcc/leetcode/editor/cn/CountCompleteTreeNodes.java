90cce4b06c5416fef1baa57762a9e2b0dcf99566
oddcc <skycc71640@gmail.com>
2020-11-24 14:59:10 +0800

    222 利用完全二叉树的性质

--- src/main/java/com/oddcc/leetcode/editor/cn/CountCompleteTreeNodes.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/CountCompleteTreeNodes.java
@@ -14,26 +14,39 @@ public class CountCompleteTreeNodes {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        private int count = 0;
-
         // 思路1，返回完全二叉树有多少个节点，各种遍历方法
         // 深度优先遍历
         // 深度优先遍历非递归，前序遍历
         // 广度优先遍历
+        // 思路2，利用完全二叉树的特性，如果是一颗满二叉树的话，假设高度为h，则节点数为2^h - 1个
+        // 如何求一颗完全二叉树的高度呢？一路顺着左子树走，走到底就是完全二叉树的高度
+        // 如果左子树的高度为left，右子树的高度为right，当left==right时，左子树是一颗高度为left的满二叉树
+        // 当left>right时，右子树是一颗高度为right的满二叉树
+        // 从上面可以观察到，总是可以求出某一边子树的节点数量，而剩下的不是满二叉树的一边，可以递归进行上述计算
         public int countNodes(TreeNode root) {
+            return count(root);
+        }
+
+        private int count(TreeNode root) {
             if (root == null) return 0;
-            List<TreeNode> nodeList = new ArrayList<>();
-            nodeList.add(root);
-            while (!nodeList.isEmpty()) {
-                List<TreeNode> nextLayer = new ArrayList<>();
-                for (TreeNode n: nodeList) {
-                    count++;
-                    if (n.left != null) nextLayer.add(n.left);
-                    if (n.right != null) nextLayer.add(n.right);
-                }
-                nodeList = nextLayer;
+            if (root.left == null && root.right == null) return 1;
+            int leftHeight = getHeight(root.left);
+            int rightHeight = getHeight(root.right);
+            if (leftHeight == rightHeight) {
+                int lH = (int) (Math.pow(2, leftHeight) - 1);
+                return lH + count(root.right) + 1;
             }
-            return count;
+            int rH = (int) (Math.pow(2, rightHeight) - 1);
+            return count(root.left) + rH + 1;
+        }
+
+        private int getHeight(TreeNode node) {
+            int h = 0;
+            while (node != null) {
+                node = node.left;
+                h++;
+            }
+            return h;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
