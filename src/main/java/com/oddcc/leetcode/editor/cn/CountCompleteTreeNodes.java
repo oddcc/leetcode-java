4b4a75b195a0f22d11ac5cf9c1df8375cd709201
oddcc <skycc71640@gmail.com>
2020-11-24 12:04:23 +0800

    222 dfs前序遍历二叉树，非递归

--- src/main/java/com/oddcc/leetcode/editor/cn/CountCompleteTreeNodes.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/CountCompleteTreeNodes.java
@@ -4,6 +4,8 @@ package com.oddcc.leetcode.editor.cn;
 
 import com.oddcc.leetcode.editor.cn.common.TreeNode;
 
+import java.util.*;
+
 public class CountCompleteTreeNodes {
     public static void main(String[] args) {
         Solution solution = new CountCompleteTreeNodes().new Solution();
@@ -14,17 +16,29 @@ public class CountCompleteTreeNodes {
     class Solution {
         private int count = 0;
 
-        // 思路1，返回完全二叉树有多少个节点，用深度遍历或广度遍历计数
+        // 思路1，返回完全二叉树有多少个节点，各种遍历方法
+        // 深度优先遍历
+        // 深度优先遍历非递归 -> 栈
+        // todo 广度优先遍历
         public int countNodes(TreeNode root) {
-            dfs(root);
+            if (root == null) return 0;
+            Deque<TreeNode> stack = new LinkedList<>();
+            stack.add(root);
+            while (!stack.isEmpty()) {
+                Deque<TreeNode> s2 = new LinkedList<>();
+                TreeNode node = stack.pop();
+                s2.push(node);
+                while (node.left != null) {
+                    s2.push(node.left);
+                    node = node.left;
+                }
+                while (!s2.isEmpty()) {
+                    TreeNode n = s2.pop();
+                    if (n.right != null) stack.push(n.right);
+                    count++;
+                }
+            }
             return count;
-        }
-
-        private void dfs(TreeNode root) {
-            if (root == null) return;
-            count++;
-            if (root.left != null) dfs(root.left);
-            if (root.right != null) dfs(root.right);
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
