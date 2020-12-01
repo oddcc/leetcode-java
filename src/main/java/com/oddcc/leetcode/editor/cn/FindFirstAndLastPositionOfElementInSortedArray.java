61e2d7ebcace55cfd22e9f64bf65aa3ee5495610
oddcc <skycc71640@gmail.com>
2020-12-01 15:20:47 +0800

    34 每日打卡，二分查找最左最右元素

--- src/main/java/com/oddcc/leetcode/editor/cn/FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -14,26 +14,63 @@ public class FindFirstAndLastPositionOfElementInSortedArray {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         // 思路1，O(N)的解法
+        // 思路2，优化查找查找左边和右边的时候，都可以使用二分查找来减少时间复杂度；分别是左边->找第一个出现的target，右边->找最后一个出现的target
         public int[] searchRange(int[] nums, int target) {
-            int start = -1;
-            int end = -1;
-            for (int i = 0; i < nums.length; i++) {
-                // 先找左边
-                if (start == -1) {
-                    if (nums[i] != target) continue;
-                    start = i;
-                    end = i;
-                }
-                else {
-                    // 再找右边
-                    if (nums[i] == target) {
-                        end = i;
-                        continue;
-                    }
-                    break;
-                }
+            if (nums.length == 0) return new int[]{-1, -1};
+            int start = findFirst(nums, 0, nums.length - 1, target);
+            System.out.println(start);
+            if (start == -1) return new int[]{-1, -1};
+            int end = findLast(nums, start, nums.length - 1, target);
+            return new int[]{start, end};
+        }
+
+        /**
+         * 二分查找第一个出现的target，找不到则返回-1
+         * @param nums
+         * @param start
+         * @param end
+         * @param target
+         * @return
+         */
+        private int findFirst(int[] nums, int start, int end, int target) {
+            if (start >= end) {
+                if (nums[start] == target) return start;
+                return -1;
             }
-            return start != -1 ? new int[]{start, end} : new int[]{-1, -1};
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < target) {
+                return findFirst(nums, mid + 1, end, target);
+            }
+            else {
+                return findFirst(nums, start, mid, target);
+            }
+        }
+
+        /**
+         * 二分查找最后一个出现的target，找不到则返回-1
+         * @param nums
+         * @param start
+         * @param end
+         * @param target
+         * @return
+         */
+        private int findLast(int[] nums, int start, int end, int target) {
+            if (start >= end) {
+                if (nums[start] == target) return start;
+                return -1;
+            }
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < target) {
+                return findLast(nums, mid + 1, end, target);
+            }
+            if (nums[mid] == target) {
+                int i = findLast(nums, mid + 1, end, target);
+                if (i != -1) return i;
+                return mid;
+            }
+            else {
+                return findLast(nums, start, mid - 1, target);
+            }
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
