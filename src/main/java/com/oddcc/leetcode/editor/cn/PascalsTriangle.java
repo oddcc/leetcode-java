bb6a29ee3f718e8c9f427c6a037fb07b226a101c
oddcc <skycc71640@gmail.com>
2020-12-04 11:32:52 +0800

    118每日打卡

--- src/main/java/com/oddcc/leetcode/editor/cn/PascalsTriangle.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/PascalsTriangle.java
@@ -38,36 +38,33 @@ public class PascalsTriangle {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        // 其实可以左对齐，比较好观察，下一行的第i个数，就是上一行的第i个+第i-1个，另外还要处理一些越界的情况
         public List<List<Integer>> generate(int numRows) {
-            List<List<Integer>> result = new ArrayList<>();
-            List<Integer> lastRow = new ArrayList<>();
+            List<List<Integer>> ans = new ArrayList<>();
             for (int i = 0; i < numRows; i++) {
-                List<Integer> row = new ArrayList<>();
-                for (int j = 0; j <= i; j++) {
-                    if (j == 0 || j == i) {
-                        row.add(1);
-                    }
-                    else {
-                        row.add(getSumFromLast(lastRow, j));
-                    }
+                if (i == 0) {
+                    List<Integer> row = new ArrayList<>();
+                    row.add(1);
+                    ans.add(row);
                 }
-                lastRow = row;
-                result.add(row);
+                else {
+                    List<Integer> pre = ans.get(i - 1);
+                    List<Integer> row = new ArrayList<>();
+                    for (int j = 0; j < i + 1; j++) {
+                        int num = 0;
+                        if (j == 0) {
+                            num = pre.get(0);
+                        }
+                        else {
+                            num = (j < pre.size() ? pre.get(j) : 0) + (j - 1 < pre.size() ? pre.get(j - 1) : 0);
+                        }
+                        row.add(num);
+                    }
+                    ans.add(row);
+                }
             }
-            return result;
-        }
-
-        // 返回lastRow中i 和 i - 1位置的和，如果越界则为0
-        private Integer getSumFromLast(List<Integer> lastRow, int i) {
-            if (lastRow.isEmpty()) return 1;
-            int lower = 0;
-            int higher = lastRow.size() - 1;
-            int a = (i >= lower && i <= higher) ? lastRow.get(i) : 0;
-            int b = (i - 1 >= lower && i - 1 <= higher) ? lastRow.get(i - 1) : 0;
-            return a + b;
+            return ans;
         }
     }
-//leetcode submit region end(Prohibit modification and deletion)
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }
