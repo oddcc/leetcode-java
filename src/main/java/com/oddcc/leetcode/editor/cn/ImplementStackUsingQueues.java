495586bf1e43b9074c3735b0757e9dd2680ad9d2
oddcc <skycc71640@gmail.com>
2020-12-06 20:20:48 +0800

    225 一个queue实现stack

--- src/main/java/com/oddcc/leetcode/editor/cn/ImplementStackUsingQueues.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/ImplementStackUsingQueues.java
@@ -2,46 +2,41 @@
 
 package com.oddcc.leetcode.editor.cn;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 public class ImplementStackUsingQueues {
     //leetcode submit region begin(Prohibit modification and deletion)
     class MyStack {
-        Queue<Integer> q1;
-        Queue<Integer> q2;
+        Queue<Integer> queue;
+
         /** Initialize your data structure here. */
         public MyStack() {
-            q1 = new LinkedList<>();
-            q2 = new LinkedList<>();
+            queue = new LinkedList<>();
         }
 
         /** Push element x onto stack. */
         public void push(int x) {
-            q2.add(x);
-            while (!q1.isEmpty()) {
-                q2.add(q1.remove());
+            int size = queue.size();
+            queue.add(x);
+            for (int i = 0; i < size; i++) {
+                queue.add(queue.remove());
             }
-            Queue<Integer> tmp = q1;
-            q1 = q2;
-            q2 = tmp;
         }
 
         /** Removes the element on top of the stack and returns that element. */
         public int pop() {
-            return q1.remove();
+            return queue.remove();
         }
 
         /** Get the top element. */
         public int top() {
-            return q1.element();
+            return queue.element();
         }
 
         /** Returns whether the stack is empty. */
         public boolean empty() {
-            return q1.isEmpty();
+            return queue.isEmpty();
         }
     }
 
