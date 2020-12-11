c4aec130884efbbe881051646b5d754d17d23066
oddcc <skycc71640@gmail.com>
2020-12-11 22:41:48 +0800

    217 每日打卡

--- src/main/java/com/oddcc/leetcode/editor/cn/ContainsDuplicate.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/ContainsDuplicate.java
@@ -1,36 +1,27 @@
+// 217
+
 package com.oddcc.leetcode.editor.cn;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ContainsDuplicate {
     public static void main(String[] args) {
         Solution solution = new ContainsDuplicate().new Solution();
+        System.out.println(solution.containsDuplicate(new int[]{1, 2, 3, 1}));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        // 思路1，记录访问过的数，哈希表；时间复杂度O(n)，空间复杂度O(n)
-        // 思路2，先排序，然后从头遍历，时间复杂度O(nlogn)，空间复杂度取决于排序算法的实现
         public boolean containsDuplicate(int[] nums) {
-            Arrays.sort(nums);
-            Integer tmp = null;
+            Set<Integer> cache = new HashSet<>();
             for (int i : nums) {
-                if (tmp == null) {
-                    tmp = i;
-                    continue;
-                }
-                else {
-                    if (tmp == i) {
-                        return true;
-                    }
-                    tmp = i;
-                }
+                if (cache.contains(i)) return true;
+                cache.add(i);
             }
             return false;
         }
     }
-//leetcode submit region end(Prohibit modification and deletion)
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }
