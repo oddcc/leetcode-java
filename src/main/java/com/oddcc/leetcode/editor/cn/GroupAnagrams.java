54809ea7b8302238994ee7e48da2a5466c4ae552
oddcc <skycc71640@gmail.com>
2020-12-12 11:47:48 +0800

    49 每日打卡，List<Integer>作为key，插入位置作为value

--- src/main/java/com/oddcc/leetcode/editor/cn/GroupAnagrams.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/GroupAnagrams.java
@@ -1,3 +1,5 @@
+// 49
+
 package com.oddcc.leetcode.editor.cn;
 
 import java.util.*;
@@ -5,32 +7,40 @@ import java.util.*;
 public class GroupAnagrams {
     public static void main(String[] args) {
         Solution solution = new GroupAnagrams().new Solution();
-//        System.out.println(solution.groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}));
-        System.out.println(solution.groupAnagrams(new String[]{"",""}));
+        System.out.println(solution.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+        System.out.println(solution.groupAnagrams(new String[]{""}));
+        System.out.println(solution.groupAnagrams(new String[]{"a"}));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        // 思路1，暴力法，扫描并用哈希表存储（排序后作为key），时间复杂度O(n)，空间复杂度O(n)
+        // 思路1，利用每个字符串的字母计数作为key来判断对应的anagram是否出现过；因为int[]默认的equals方法是比较对地址，所以用ArrayList，并在value中记录对应的位置
         public List<List<String>> groupAnagrams(String[] strs) {
-            if (strs.length == 0) return new ArrayList<>();
-            Map<String, List<String>> cache = new HashMap<>();
-            for (String s: strs) {
-                char[] chars = s.toCharArray();
-                Arrays.sort(chars);
-                String key = String.copyValueOf(chars);
-                if (!cache.containsKey(key)) {
-                    cache.put(key, new ArrayList<>());
-                }
-                cache.get(key).add(s);
-            }
             List<List<String>> ans = new ArrayList<>();
-            for (String key: cache.keySet()) {
-                ans.add(new ArrayList<>(cache.get(key)));
+            Map<List<Integer>, Integer> cache = new HashMap<>();
+            int i = 0;
+            for (String str : strs) {
+                int[] charCount = new int[26];
+                for (char c : str.toCharArray()) {
+                    charCount[c - 'a']++;
+                }
+                List<Integer> charCountList = new ArrayList<>();
+                for (int x : charCount) {
+                    charCountList.add(x);
+                }
+                if (cache.containsKey(charCountList)) {
+                    ans.get(cache.get(charCountList)).add(str);
+                }
+                else {
+                    List<String> tmp = new ArrayList<>();
+                    tmp.add(str);
+                    ans.add(tmp);
+                    cache.put(charCountList, i++);
+                }
             }
             return ans;
         }
     }
-//leetcode submit region end(Prohibit modification and deletion)
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }
