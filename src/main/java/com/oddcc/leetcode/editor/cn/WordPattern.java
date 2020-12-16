9e8eac3e2f5f60acf5c87264ad35e3b96beac938
oddcc <skycc71640@gmail.com>
2020-12-16 14:52:23 +0800

    290 每日打卡，利用双射的性质，遍历两个字符串的同时建立映射关系，遇到不符则说明有问题

--- src/main/java/com/oddcc/leetcode/editor/cn/WordPattern.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/WordPattern.java
@@ -3,9 +3,7 @@
 package com.oddcc.leetcode.editor.cn;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class WordPattern {
     public static void main(String[] args) {
@@ -18,36 +16,23 @@ public class WordPattern {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         // 思路1，有多少个元素出现，元素的位置是什么；都求出来之后再进行比较
+        // 思路2，题目中提到了bijection，双射
         public boolean wordPattern(String pattern, String s) {
-            Map<Character, Set<Integer>> pIndexMap = new HashMap<>();
-            char[] patternArray = pattern.toCharArray();
-            for (int i = 0; i < patternArray.length; i++) {
-                char c = patternArray[i];
-                if (pIndexMap.containsKey(c)) {
-                    pIndexMap.get(c).add(i);
+            Map<String, Character> strChar = new HashMap<>();
+            Map<Character, String> charStr = new HashMap<>();
+            char[] chars = pattern.toCharArray();
+            String[] strs = s.split(" ");
+            if (strs.length != chars.length) return false;
+            for (int i = 0; i < chars.length; i++) {
+                // 对应关系建立了就不能修改，一旦不符则说明有问题
+                if (strChar.get(strs[i]) == null) {
+                    strChar.put(strs[i], chars[i]);
                 }
-                else {
-                    Set<Integer> indexSet = new HashSet<>();
-                    indexSet.add(i);
-                    pIndexMap.put(c, indexSet);
+                if (charStr.get(chars[i]) == null) {
+                    charStr.put(chars[i], strs[i]);
                 }
-            }
-            Map<String, Set<Integer>> sIndexMap = new HashMap<>();
-            String[] sArray = s.split(" ");
-            for (int i = 0; i < sArray.length; i++) {
-                String str = sArray[i];
-                if (sIndexMap.containsKey(str)) {
-                    sIndexMap.get(str).add(i);
-                }
-                else {
-                    Set<Integer> indexSet = new HashSet<>();
-                    indexSet.add(i);
-                    sIndexMap.put(str, indexSet);
-                }
-            }
-            if (sIndexMap.size() != pIndexMap.size()) return false;
-            for (int i = 0; i < sArray.length; i++) {
-                if (!sIndexMap.get(sArray[i]).equals(pIndexMap.get(patternArray[i]))) return false;
+                if (!strChar.get(strs[i]).equals(chars[i])) return false;
+                if (!charStr.get(chars[i]).equals(strs[i])) return false;
             }
             return true;
         }
