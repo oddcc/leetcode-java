0f10bfda9962f71585c25191c4d08bccaf80f814
oddcc <skycc71640@gmail.com>
2020-12-17 23:46:40 +0800

    237 每日打卡

---
+++ src/main/java/com/oddcc/leetcode/editor/cn/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -0,0 +1,23 @@
+// 714
+
+package com.oddcc.leetcode.editor.cn;
+
+public class BestTimeToBuyAndSellStockWithTransactionFee {
+    public static void main(String[] args) {
+        Solution solution = new BestTimeToBuyAndSellStockWithTransactionFee().new Solution();
+        // System.out.println(solution.maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
+        System.out.println(solution.maxProfit(new int[]{1, 4, 6, 2, 8, 3, 10, 14}, 3));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maxProfit(int[] prices, int fee) {
+            // int len = prices.length;
+            //
+            // return dp[0][len - 1];
+            return 0;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/com/oddcc/leetcode/editor/cn/DeleteNodeInALinkedList.java
@@ -0,0 +1,28 @@
+// 237
+
+package com.oddcc.leetcode.editor.cn;
+
+import com.oddcc.leetcode.editor.cn.common.ListNode;
+
+public class DeleteNodeInALinkedList {
+    public static void main(String[] args) {
+        Solution solution = new DeleteNodeInALinkedList().new Solution();
+        ListNode n1 = ListNode.GetNodeList(4,5,1,9);
+        solution.deleteNode(n1.next);
+        System.out.println(n1);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        // 题目不给访问头节点，那只能重新赋值了吧，否则没法实现
+        public void deleteNode(ListNode node) {
+            while (node.next.next != null) {
+                node.val = node.next.val;
+                node = node.next;
+            }
+            node.val = node.next.val;
+            node.next = null;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
