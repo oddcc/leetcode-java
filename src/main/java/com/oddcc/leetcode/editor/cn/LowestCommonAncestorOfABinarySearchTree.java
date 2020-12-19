341eb4091b4d178db5173b71d010b0168beddff1
oddcc <skycc71640@gmail.com>
2020-12-19 22:01:01 +0800

    235 利用二叉搜索树的性质，一次遍历

--- src/main/java/com/oddcc/leetcode/editor/cn/LowestCommonAncestorOfABinarySearchTree.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/LowestCommonAncestorOfABinarySearchTree.java
@@ -4,9 +4,6 @@ package com.oddcc.leetcode.editor.cn;
 
 import com.oddcc.leetcode.editor.cn.common.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LowestCommonAncestorOfABinarySearchTree {
     public static void main(String[] args) {
         Solution solution = new LowestCommonAncestorOfABinarySearchTree().new Solution();
@@ -20,39 +17,24 @@ public class LowestCommonAncestorOfABinarySearchTree {
     // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         // 思路1，两个节点向上遍历
+        // 思路2，利用二叉搜索树的性质，通过比较大小，可以确定在遍历过程中遍历的顺序
+        // 如果是p, root, q那共同肯定是root
+        // 如果是p, q在同一侧则从根节点向所在侧遍历，如果节点n在q、p之间，则答案是n，否则继续判断向哪个方向遍历
         public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-            List<TreeNode> parentListP = getPath(root, p, new ArrayList<>());
-            List<TreeNode> parentListQ = getPath(root, q, new ArrayList<>());
-            TreeNode ans = parentListP.get(0);
-            for (int i = 0; i < Math.min(parentListP.size(), parentListQ.size()); i++) {
-                if (parentListP.get(i).val != parentListQ.get(i).val) {
-                    break;
-                }
-                ans = parentListP.get(i);
+            if (isMid(root, p, q)) return root;
+            // 因为遍历的时候都判断过方向，所以遇到相同p或q就可以可以
+            if (root.val == p.val) return p;
+            if (root.val == q.val) return q;
+            // 在左侧
+            if (p.val < root.val && q.val < root.val) {
+                return lowestCommonAncestor(root.left, p, q);
             }
-            return ans;
+            // 在右侧
+            return lowestCommonAncestor(root.right, p, q);
         }
 
-        private List<TreeNode> getPath(TreeNode current, TreeNode target, ArrayList<TreeNode> path) {
-            if (current.val == target.val) {
-                path.add(target);
-                // System.out.println(path);
-                return new ArrayList<>(path);
-            }
-            if (current.left == null && current.right == null) return null;
-            if (current.left != null) {
-                path.add(current);
-                List<TreeNode> path1 = getPath(current.left, target, path);
-                if (path1 != null) return path1;
-                path.remove(path.size() - 1);
-            }
-            if (current.right != null) {
-                path.add(current);
-                List<TreeNode> path2 = getPath(current.right, target, path);
-                if (path2 != null) return path2;
-                path.remove(path.size() - 1);
-            }
-            return null;
+        private boolean isMid(TreeNode root, TreeNode p, TreeNode q) {
+            return (p.val < root.val && root.val < q.val) || (q.val < root.val && root.val < p.val);
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
