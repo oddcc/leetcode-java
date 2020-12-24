24db3be34d8062465b889faba12de8d5422da7ee
oddcc <skycc71640@gmail.com>
2020-12-24 14:34:01 +0800

    122 动态规划，类似714

--- src/main/java/com/oddcc/leetcode/editor/cn/BestTimeToBuyAndSellStockIi.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/BestTimeToBuyAndSellStockIi.java
@@ -11,29 +11,19 @@ public class BestTimeToBuyAndSellStockIi {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        // 多次买卖，最大值；找上升区间，在每个上升区间的start买入，end卖出
+        // 思路1，动态规划
         public int maxProfit(int[] prices) {
-            if (prices.length <= 1) return 0;
-            int start = 0;
-            int end = 1;
-            int ans = 0;
-            // end到结尾时遍历完成
-            while (end < prices.length) {
-                // 每一趟都尽量把end往后移
-                while (end < prices.length && prices[end] > prices[end - 1]) {
-                    end++;
-                }
-                // 不满足时，end - 1指向的是区间的end
-                // 只有start<end时才是有效的，如果不满足的end继续后移
-                if (start < end) {
-                    ans += prices[end - 1] - prices[start];
-                    start = end;
-                }
-                else {
-                    end++;
-                }
+            int len = prices.length;
+            int[][] dp = new int[len][2]; // dp[i][0]表示第i天结束时手中没有股票的收益，dp[i][1]表示第i天结束时手中有股票的收益
+            dp[0][0] = 0; // 第一天手中没股票，收益为0
+            dp[0][1] = -prices[0]; // 第一天手中有股票，收益为负，值是买股票花的钱
+            for (int i = 1; i < len; i++) {
+                //                  i-1天结束时手里有股票，i天卖出   i-1天手里没有股票，i天什么也没做
+                dp[i][0] = Math.max(dp[i - 1][1] + prices[i], dp[i - 1][0]);
+                //                  i-1天手里没股票，i天买入   i-1天手里有股票，i天什么也没做
+                dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
             }
-            return ans;
+            return Math.max(dp[len - 1][0], dp[len - 1][1]);
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
--- src/main/java/com/oddcc/leetcode/editor/cn/BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -14,11 +14,13 @@ public class BestTimeToBuyAndSellStockWithTransactionFee {
         // 思路1，动态规划
         public int maxProfit(int[] prices, int fee) {
             int len = prices.length;
-            int[][] dp = new int[len][2];
-            dp[0][0] = 0;
-            dp[0][1] = -prices[0];
+            int[][] dp = new int[len][2]; // dp[i][0]表示第i天结束时手中没有股票的收益，dp[i][1]表示第i天结束时手中有股票的收益
+            dp[0][0] = 0; // 第一天手中没股票，收益为0
+            dp[0][1] = -prices[0]; // 第一天手中有股票，收益为负，值是买股票花的钱
             for (int i = 1; i < len; i++) {
+                //                  i-1天结束时手里有股票，i天卖出   i-1天手里没有股票，i天什么也没做
                 dp[i][0] = Math.max(dp[i - 1][1] + prices[i] - fee, dp[i - 1][0]);
+                //                  i-1天手里没股票，i天买入   i-1天手里有股票，i天什么也没做
                 dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
             }
             return Math.max(dp[len - 1][0], dp[len - 1][1]);
