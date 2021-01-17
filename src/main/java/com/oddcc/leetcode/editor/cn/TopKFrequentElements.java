09161a69e923110d56b34b520c8c85d5d5e7a2c3
oddcc <skycc71640@gmail.com>
2021-01-17 14:36:33 +0800

    347 代码优化

--- src/main/java/com/oddcc/leetcode/editor/cn/TopKFrequentElements.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/TopKFrequentElements.java
@@ -9,15 +9,17 @@ public class TopKFrequentElements {
         Solution solution = new TopKFrequentElements().new Solution();
         System.out.println(Arrays.toString(solution.topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2)));
         System.out.println(Arrays.toString(solution.topKFrequent(new int[]{1}, 1)));
+        System.out.println(Arrays.toString(solution.topKFrequent(new int[]{4, 1, -1, 2, -1, 2, 3}, 2)));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         // 要求时间复杂度至少是O(NlogN)
-        // 思路1，利用哈希表存元素数量，利用容量为k的最大堆来取第k大的数，遍历的同时，不停的入堆，最后再从堆中弹出第k个元素，就是答案
+        // 思路1，利用哈希表存元素数量，利用容量为k的最小堆来取第k大的数，遍历的同时，不停的入堆，最后再从堆中弹出第k个元素，就是答案
+        // 思路2，代码优化
         public int[] topKFrequent(int[] nums, int k) {
             Map<Integer, Integer> countMap = new HashMap<>();
-            PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.comparingInt(countMap::get));
+            PriorityQueue<Integer> minHeap = new PriorityQueue<>(Comparator.comparingInt(countMap::get));
             for (int i : nums) {
                 if (countMap.containsKey(i)) {
                     countMap.put(i, countMap.get(i) + 1);
@@ -28,16 +30,21 @@ public class TopKFrequentElements {
             }
 
             for (int i : countMap.keySet()) {
-                System.out.println("add " + i);
-                maxHeap.add(i);
-                if (maxHeap.size() > k) {
-                    System.out.println("remove " + maxHeap.remove());
+                if (minHeap.size() < k) {
+                    System.out.println("add " + i);
+                    minHeap.add(i);
+                }
+                else {
+                    if (countMap.get(i) < countMap.get(minHeap.peek())) continue;
+                    System.out.println("add " + i);
+                    minHeap.add(i);
+                    System.out.println("remove " + minHeap.remove());
                 }
             }
 
             int[] ans = new int[k];
-            for (int i = k - 1; i >= 0; i--) {
-                int n = maxHeap.remove();
+            for (int i = 0; i < k; i++) {
+                int n = minHeap.remove();
                 System.out.println("retrieve " + n);
                 ans[i] = n;
             }
