006914c596a748061a0d1d58f83f46f45b5c791f
oddcc <skycc71640@gmail.com>
2021-01-27 11:32:05 +0800

    538 代码优化，用右根左的顺序遍历，直接得到降序排列

--- src/main/java/com/oddcc/leetcode/editor/cn/ConvertBstToGreaterTree.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/ConvertBstToGreaterTree.java
@@ -16,34 +16,22 @@ public class ConvertBstToGreaterTree {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        private int count = 0;
+        private int sum = 0;
+
         // 思路1，BST的中序遍历得到的是升序结果，我们得到这个数组后，可以直接求出每个节点处理后的值
         // 有了之后的值，可以通过再一次中序遍历，把值赋值回去，就完成了
+        // 思路2，如果按右根左的顺序遍历，直接得到的就是降序排列
         public TreeNode convertBST(TreeNode root) {
-            List<Integer> result = new ArrayList<>();
-            dfs(root, result);
-            System.out.println(result);
-            int size = result.size();
-            for (int i = size - 1; i >= 0; i--) {
-                if (i < size - 1) result.set(i, result.get(i + 1) + result.get(i));
-            }
-            System.out.println(result);
-            dfs2(root, result);
+            dfs(root);
             return root;
         }
 
-        private void dfs2(TreeNode root, List<Integer> result) {
+        private void dfs(TreeNode root) {
             if (root == null) return;
-            dfs2(root.left, result);
-            root.val = result.get(count++);
-            dfs2(root.right, result);
-        }
-
-        private void dfs(TreeNode root, List<Integer> result) {
-            if (root == null) return;
-            dfs(root.left, result);
-            result.add(root.val);
-            dfs(root.right, result);
+            dfs(root.right);
+            root.val += sum;
+            sum = root.val;
+            dfs(root.left);
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
