2665056af18233226cc89a3ba9f9e9fe27afbabc
oddcc <skycc71640@gmail.com>
2021-02-01 18:56:43 +0800

    84 利用单调栈来记录第一个小于heights[i]的值的位置，从而消除大量重复计算

--- src/main/java/com/oddcc/leetcode/editor/cn/LargestRectangleInHistogram.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/LargestRectangleInHistogram.java
@@ -2,6 +2,9 @@
 
 package com.oddcc.leetcode.editor.cn;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class LargestRectangleInHistogram {
     public static void main(String[] args) {
         Solution solution = new LargestRectangleInHistogram().new Solution();
@@ -15,38 +18,47 @@ public class LargestRectangleInHistogram {
         // 如何求每个矩形面积呢？
         // 比如[2,1,5,6,2,3]中，当index=4，value=2时，高是2，而宽应该是4（2~5）
         // 其中2是左边最后一个大于等于2的值的索引，而5是右边最后一个大于等于2的值的索引，注意这个搜索必须连续，如果中断了就不算
+        // 思路2，在确定宽度时，包含了大量的重复计算，如何优化？
+        // 连续的找下一个大于等于x的索引，等同于找下一个小于x的索引
+        // 如果对heights[i]能快速找到左边第一个小于heights[i]的值的索引left，找到右边第一个小于heights[i]的值的索引right，则问题就好解了
         public int largestRectangleArea(int[] heights) {
+            int len = heights.length;
+            int[] leftFirstLower = new int[len];
+            Deque<int[]> stack = new LinkedList<>();
+            for (int i = 0; i < len; i++) {
+                if (!stack.isEmpty()) {
+                    int[] n = stack.peek();
+                    while (n != null && heights[i] < n[0] && i > n[1]) {
+                        leftFirstLower[n[1]] = i - n[1];
+                        stack.pop();
+                        n = stack.peek();
+                    }
+                }
+                stack.push(new int[]{heights[i], i});
+            }
+            while (!stack.isEmpty()) leftFirstLower[stack.pop()[1]] = 0;
+            int[] rightFirstLower = new int[len];
+            for (int i = len - 1; i >= 0; i--) {
+                if (!stack.isEmpty()) {
+                    int[] n = stack.peek();
+                    while (n != null && heights[i] < n[0] && i < n[1]) {
+                        rightFirstLower[n[1]] = n[1] - i;
+                        stack.pop();
+                        n = stack.peek();
+                    }
+                }
+                stack.push(new int[]{heights[i], i});
+            }
+            while (!stack.isEmpty()) rightFirstLower[stack.pop()[1]] = 0;
             int ans = 0;
-            for (int i = 0; i < heights.length; i++) {
-                ans = Math.max(ans, calArea(heights, i));
+            for (int i = 0; i < len; i++) {
+                int h = heights[i];
+                int left = rightFirstLower[i] == 0? 0: i - rightFirstLower[i] + 1;
+                int right = leftFirstLower[i] == 0? len - 1: i + leftFirstLower[i] - 1;
+                int w = right - left + 1;
+                ans = Math.max(ans, h * w);
             }
             return ans;
-        }
-
-        // 以索引i为中心，求对应矩形面积
-        private int calArea(int[] heights, int i) {
-            int h = heights[i];
-            int left = i - 1;
-            while (left >= 0 && heights[left] >= heights[i]) left--;
-            int right = i + 1;
-            while (right < heights.length && heights[right] >= heights[i]) right++;
-            // 如果left停留在0的左边，说明0的位置都大于等于heights[i]
-            if (left < 0) {
-                left = 0;
-            }
-            // left停留的位置是最后一个符合条件的值的左边，需要右移一位
-            else {
-                left++;
-            }
-            // 如果right停留在len - 1的右边，说明len-1的位置都大于等于heights[i]
-            if (right >= heights.length) {
-                right = heights.length - 1;
-            }
-            // right停留的位置是最后一个符合条件的值的右边，需要左移一位
-            else {
-                right--;
-            }
-            return h * (right - left + 1);
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
