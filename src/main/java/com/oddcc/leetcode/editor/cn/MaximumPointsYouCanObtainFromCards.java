449b54f5be775186f6502b2a9ba2532f2ec6ea60
oddcc <skycc71640@gmail.com>
2021-02-06 11:23:41 +0800

    1423 把求最大值的问题转化为求最小值的问题，利用滑动窗口

--- src/main/java/com/oddcc/leetcode/editor/cn/MaximumPointsYouCanObtainFromCards.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/MaximumPointsYouCanObtainFromCards.java
@@ -5,39 +5,42 @@ package com.oddcc.leetcode.editor.cn;
 public class MaximumPointsYouCanObtainFromCards {
     public static void main(String[] args) {
         Solution solution = new MaximumPointsYouCanObtainFromCards().new Solution();
-        // System.out.println(solution.maxScore(new int[]{1, 2, 3, 4, 5, 6, 1}, 3));
-        // solution.max = 0;
-        // System.out.println(solution.maxScore(new int[]{2, 2, 2}, 2));
-        // solution.max = 0;
-        // System.out.println(solution.maxScore(new int[]{9, 7, 7, 9, 7, 7, 9}, 7));
-        // solution.max = 0;
-        // System.out.println(solution.maxScore(new int[]{1, 1000, 1}, 1));
-        // solution.max = 0;
-        // System.out.println(solution.maxScore(new int[]{1, 79, 80, 1, 1, 1, 200, 1}, 3));
-        solution.max = 0;
+        System.out.println(solution.maxScore(new int[]{1, 2, 3, 4, 5, 6, 1}, 3));
+        System.out.println(solution.maxScore(new int[]{2, 2, 2}, 2));
+        System.out.println(solution.maxScore(new int[]{9, 7, 7, 9, 7, 7, 9}, 7));
+        System.out.println(solution.maxScore(new int[]{1, 1000, 1}, 1));
+        System.out.println(solution.maxScore(new int[]{1, 79, 80, 1, 1, 1, 200, 1}, 3));
         System.out.println(solution.maxScore(new int[]{99, 82, 25, 56, 39, 77, 22, 58, 64, 77, 19, 36, 93, 14, 19, 12, 94, 76, 93, 24, 92, 67, 18, 37, 37, 60, 87, 28, 64, 7, 29}, 27));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        private int max = 0;
-
-        // 思路1，回溯法，利用数组记录选择PATH，并更新最终结果
+        // 思路1，深度搜索回溯法，利用数组记录选择PATH，并更新最终结果
+        // 思路2，求最大值的问题，转化为求最小值的问题，因为每次从两端取值，最终剩下的是数组中的一个连续的子数组，且子数组的长度是已知的
+        // 可以利用滑动窗口求子数组的最小和，减掉总和，就是最大值
         public int maxScore(int[] cardPoints, int k) {
             int len = cardPoints.length;
-            search(cardPoints, k, 1, 1, len - 1, cardPoints[0]);
-            search(cardPoints, k, 1, 0, len - 2, cardPoints[len - 1]);
-            return max;
-        }
-
-        // start和end是取值范围，下一步可以取其中之一
-        private void search(int[] cardPoints, int k, int pathLen, int start, int end, int sum) {
-            if (pathLen >= k || start > end) {
-                max = Math.max(sum, max);
-                return;
+            int sum = 0;
+            int subSum = 0;
+            int min = Integer.MAX_VALUE;
+            int right = (len - k) - 1;
+            while (right < len) {
+                if (right == (len - k) - 1) {
+                    for (int i = 0; i <= (len - k) - 1; i++) {
+                        sum += cardPoints[i];
+                    }
+                    subSum = sum;
+                    min = subSum;
+                }
+                else {
+                    sum += cardPoints[right];
+                    subSum += cardPoints[right];
+                    subSum -= cardPoints[right - (len - k) + 1 - 1];
+                    min = Math.min(subSum, min);
+                }
+                right++;
             }
-            search(cardPoints, k, pathLen + 1, start + 1, end, sum + cardPoints[start]);
-            search(cardPoints, k, pathLen + 1, start, end - 1, sum + cardPoints[end]);
+            return sum - min;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
