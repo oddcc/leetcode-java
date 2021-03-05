d0ad1bb36151ccfaee48395d7c93b2e143b78b00
oddcc <skycc71640@gmail.com>
2021-03-05 11:24:53 +0800

    55 动态规划

--- src/main/java/com/oddcc/leetcode/editor/cn/JumpGame.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/JumpGame.java
@@ -1,40 +1,33 @@
+// 55
+
 package com.oddcc.leetcode.editor.cn;
 
 public class JumpGame {
     public static void main(String[] args) {
         Solution solution = new JumpGame().new Solution();
         System.out.println(solution.canJump(new int[]{2, 3, 1, 1, 4}));
-//        System.out.println(solution.canJump(new int[]{3, 2, 1, 0, 4}));
-//        System.out.println(solution.canJump(new int[]{2, 1}));
+       System.out.println(solution.canJump(new int[]{3, 2, 1, 0, 4}));
+       System.out.println(solution.canJump(new int[]{2, 1}));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        // 思路1，动态规划
+        // 思路1，遍历，每个位置跳每种长度的方式都进行计算，找到一个就算找到答案，如果都没有则返回false
+        // 思路1包含太多的重复运算
+        // 思路2，动态规划，dp[i]表示从0出发，能否到达i
         public boolean canJump(int[] nums) {
-            // status[i]的意义是，从这个位置开始，能不能达到终点；0-不知道，1-不能达到终点，2-能达到终点
-            int[] status = new int[nums.length];
-            return canJumpToEnd(0, nums, status);
-        }
-
-        private boolean canJumpToEnd(int start, int[] nums, int[] status) {
-            // 如果开始的地方已经超过终点了，肯定不行
-            if (start > nums.length - 1) return false;
-            // 如果开始的地方就是终点，说明可以
-            if (start == nums.length - 1) return true;
-            boolean can = false;
-            for (int i = 1; i <= nums[start]; i++) {
-                int newStart = start + i;
-                if (newStart > nums.length - 1) break;
-                if (status[newStart] == 0) {
-                    can = can || canJumpToEnd(newStart, nums, status);
-                    status[newStart] = can ? 2 : 1;
-                }
-                else {
-                    can = can || status[newStart] == 2;
+            int len = nums.length;
+            boolean[] dp = new boolean[len];
+            dp[0] = true;
+            for (int i = 0; i < len; i++) {
+                if (!dp[i]) continue;
+                for (int j = i; j <= i + nums[i]; j++) {
+                    if (j < len) {
+                        dp[j] = true;
+                    }
                 }
             }
-            return can;
+            return dp[len - 1];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
