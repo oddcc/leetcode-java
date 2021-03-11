778998c46f889f95de7471d5cf782ee2534ab255
oddcc <skycc71640@gmail.com>
2021-03-11 20:32:20 +0800

    227 每日打卡

---
+++ src/main/java/com/oddcc/leetcode/editor/cn/BasicCalculatorIi.java
@@ -0,0 +1,51 @@
+// 227
+
+package com.oddcc.leetcode.editor.cn;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class BasicCalculatorIi {
+    public static void main(String[] args) {
+        Solution solution = new BasicCalculatorIi().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int calculate(String s) {
+            Deque<Integer> stack = new LinkedList<Integer>();
+            char preSign = '+';
+            int num = 0;
+            int n = s.length();
+            for (int i = 0; i < n; ++i) {
+                if (Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + s.charAt(i) - '0';
+                }
+                if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == n - 1) {
+                    switch (preSign) {
+                        case '+':
+                            stack.push(num);
+                            break;
+                        case '-':
+                            stack.push(-num);
+                            break;
+                        case '*':
+                            stack.push(stack.pop() * num);
+                            break;
+                        default:
+                            stack.push(stack.pop() / num);
+                    }
+                    preSign = s.charAt(i);
+                    num = 0;
+                }
+            }
+            int ans = 0;
+            while (!stack.isEmpty()) {
+                ans += stack.pop();
+            }
+            return ans;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
