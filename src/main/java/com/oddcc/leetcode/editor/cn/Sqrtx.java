23d99e576bf5c23cdd13458a36314880fc46d648
oddcc <skycc71640@gmail.com>
2021-03-18 14:42:23 +0800

    69 二分查找

--- src/main/java/com/oddcc/leetcode/editor/cn/Sqrtx.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/Sqrtx.java
@@ -33,27 +33,37 @@ public class Sqrtx {
         System.out.println(solution.mySqrt(2147395599));
         System.out.println(solution.mySqrt(6));
         System.out.println(solution.mySqrt(0));
+        System.out.println(solution.mySqrt(3));
+        System.out.println(solution.mySqrt(4));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        // 0 <= sqrt(x) < x
+        // 思路1，枚举，设置一个足够小的步进值，在0到x之间遍历
+        // 思路2，二分查找，在0到x之间二分查找，查找的应该是0到x之间的数字i，i是最后一个满足i*i < x的值
         public int mySqrt(int x) {
-            long left = 0;
-            long right = x;
-            long r = 0;
-            while (left <= right) {
-                long mid = (right - left) / 2 + left;
-                if (mid * mid <= x) {
-                    r = mid;
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+            return (int) search(0, x, x);
+        }
+
+        private long search(long left, long right, long tar) {
+            if (left >= right) {
+                if (left * left <= tar) return left;
+                return -1;
             }
-            return (int) r;
+            long mid = (right - left) / 2 + left;
+            // if (mid * mid == tar) return mid;
+            // 说明大了，要找左半部分
+            if (mid * mid > tar) {
+                return search(left, mid - 1, tar);
+            }
+            // 说明小了，要找右半部分，但不能排除mid，因为mid可能是答案
+            else {
+                long i = search(mid + 1, right, tar);
+                if (i == -1) return mid;
+                return i;
+            }
         }
     }
-//leetcode submit region end(Prohibit modification and deletion)
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }
