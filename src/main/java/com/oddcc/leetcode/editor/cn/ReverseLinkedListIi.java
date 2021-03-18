c0deb4252f45fd136de072226247b2b2b3ff9861
oddcc <skycc71640@gmail.com>
2021-03-18 12:03:19 +0800

    92 代码优化，使用递归反转中间链表

--- src/main/java/com/oddcc/leetcode/editor/cn/ReverseLinkedListIi.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/ReverseLinkedListIi.java
@@ -18,6 +18,7 @@ public class ReverseLinkedListIi {
         // 链表如何倒置？
         // 思路1，利用辅助数组，记录链表节点的位置，直接修改链表节点的值
         // 思路2，把链表分为3部分，反转中间部分，然后拼接
+        // 思路3，使用递归来反转中间部分
         public ListNode reverseBetween(ListNode head, int left, int right) {
             ListNode dummy = new ListNode(-1);
             dummy.next = head;
@@ -32,27 +33,24 @@ public class ReverseLinkedListIi {
             ListNode tmp = next.next;
             next.next = null;
             next = tmp;
+            // 到这里next获取结束
             ListNode newHead = reverse(pre.next);
-            pre.next.next = next;
+            pre.next.next = next; // 这时原本的pre.next应该在反转后的链表尾部
             pre.next = newHead;
             return dummy.next;
         }
 
+        // 递归反转链表，head表示要反转的链表头，返回反转后的链表头
         private ListNode reverse(ListNode head) {
-            ListNode pre = null;
-            while (head != null) {
-                ListNode tmp = head.next;
-                if (pre == null) {
-                    pre = head;
-                    head.next = null;
-                }
-                else {
-                    head.next = pre;
-                    pre = head;
-                }
-                head = tmp;
+            if (head.next == null) {
+                return head;
             }
-            return pre;
+            ListNode h = reverse(head.next); // 这里逻辑要求先反转
+            // 此时head还持有原来head.next的引用，我们需要把head加到链表中去
+            // 原来的head.next现在应该在反转后的链表尾部
+            head.next.next = head; // 加到反转后的链表尾部
+            head.next = null; // 断开其他连接
+            return h;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
