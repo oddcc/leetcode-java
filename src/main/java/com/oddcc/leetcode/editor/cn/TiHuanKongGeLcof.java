a56258add7c06828c71994b7d571499fd56eea3f
oddcc <skycc71640@gmail.com>
2021-03-25 16:18:38 +0800

    Offer 05 一次遍历

---
+++ src/main/java/com/oddcc/leetcode/editor/cn/TiHuanKongGeLcof.java
@@ -0,0 +1,28 @@
+// 剑指 Offer 05
+
+package com.oddcc.leetcode.editor.cn;
+
+public class TiHuanKongGeLcof {
+    public static void main(String[] args) {
+        Solution solution = new TiHuanKongGeLcof().new Solution();
+        System.out.println(solution.replaceSpace("We are happy."));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        // 思路1，直接按位读取然后替换
+        public String replaceSpace(String s) {
+            StringBuilder sb = new StringBuilder();
+            for (char c: s.toCharArray()) {
+                if (c != ' ') {
+                    sb.append(c);
+                } else {
+                    sb.append("%20");
+                }
+            }
+            return sb.toString();
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
