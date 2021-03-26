bf6a37a143afd25cf8a1c2bfc3ebd8fe8c970b3f
oddcc <skycc71640@gmail.com>
2021-03-26 10:29:49 +0800

    Offer 10-1 代码优化

--- src/main/java/com/oddcc/leetcode/editor/cn/FeiBoNaQiShuLieLcof.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/FeiBoNaQiShuLieLcof.java
@@ -8,21 +8,26 @@ public class FeiBoNaQiShuLieLcof {
         System.out.println(solution.fib(2));
         System.out.println(solution.fib(5));
         System.out.println(solution.fib(48));
+        System.out.println(solution.fib(51));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         // 思路1，动态规划提高效率
+        // 思路2，动态规划，但只保存需要的结果
         public int fib(int n) {
             if (n == 0) return 0;
             if (n == 1) return 1;
-            int[] dp = new int[n + 1];
-            dp[0] = 0;
-            dp[1] = 1;
+            int pre2 = 0;
+            int pre1 = 1;
+            int ans = 0;
             for (int i = 2; i < n + 1; i++) {
-                dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+                ans = (pre2 + pre1) % 1000000007;
+                pre2 = pre1;
+                pre1 = ans;
+
             }
-            return dp[n] % 1000000007;
+            return ans % 1000000007;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
