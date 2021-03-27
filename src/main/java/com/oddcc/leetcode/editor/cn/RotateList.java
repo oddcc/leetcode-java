71665011d114fca7d5fa19fba42f6debd47526c2
oddcc <skycc71640@gmail.com>
2021-03-27 10:08:00 +0800

    61 每日打卡，链表成环+两次遍历

--- src/main/java/com/oddcc/leetcode/editor/cn/RotateList.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/RotateList.java
@@ -4,9 +4,6 @@ package com.oddcc.leetcode.editor.cn;
 
 import com.oddcc.leetcode.editor.cn.common.ListNode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class RotateList {
     public static void main(String[] args) {
         Solution solution = new RotateList().new Solution();
@@ -18,21 +15,24 @@ public class RotateList {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         // 思路1，一次遍历+哈希表，一次遍历，记录节点的index，然后直接进行拼接
+        // 思路2，链表成环+二次遍历，第一次遍历，得到链表原来的长度len，并使链表成环
+        // 成环后再遍历len - k（k要取模）个节点，断开，并返回对应节点即可
         public ListNode rotateRight(ListNode head, int k) {
             if (k == 0 || head == null) return head;
             ListNode cur = head;
-            int len = 0;
-            Map<Integer, ListNode> cache = new HashMap<>();
-            while (cur != null) {
-                cache.put(len++, cur);
+            int len = 1;
+            while (cur.next != null) {
                 cur = cur.next;
+                len++;
             }
             k = k % (len);
             if (k == 0) return head;
-            int tarIndex = len - k;
-            ListNode ans = cache.get(tarIndex);
-            if (tarIndex - 1 >= 0) cache.get(tarIndex - 1).next = null;
-            cache.get(len - 1).next = head;
+            cur.next = head;
+            for (int i = 0; i < len - k; i++) {
+                cur = cur.next;
+            }
+            ListNode ans = cur.next;
+            cur.next = null;
             return ans;
         }
     }
