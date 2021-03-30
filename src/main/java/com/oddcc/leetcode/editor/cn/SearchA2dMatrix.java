309835041dfc3179155999ad0c719acea990bc40
oddcc <skycc71640@gmail.com>
2021-03-30 11:08:50 +0800

    74 代码优化，由递归改为循环

--- src/main/java/com/oddcc/leetcode/editor/cn/SearchA2dMatrix.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/SearchA2dMatrix.java
@@ -22,21 +22,21 @@ public class SearchA2dMatrix {
         public boolean searchMatrix(int[][] matrix, int target) {
             int m = matrix.length;
             int n = matrix[0].length;
-            int i = search(matrix, 0, m * n - 1, target);
-            return i != -1;
-        }
-
-        private int search(int[][] matrix, int left, int right, int target) {
-            if (!valid(matrix, left, right)) return -1;
-            if (left >= right && get(matrix, left) != target) return -1;
-            int mid = (right - left) / 2 + left;
-            if (get(matrix, mid) == target) {
-                return mid;
+            int ans = -1;
+            int left = 0;
+            int right = m * n - 1;
+            while (left <= right) {
+                int mid = (right - left) / 2 + left;
+                if (get(matrix, mid) > target) {
+                    right = mid - 1;
+                } else if (get(matrix, mid) < target) {
+                    left = mid + 1;
+                } else {
+                    ans = mid;
+                    break;
+                }
             }
-            if (get(matrix, mid) > target) {
-                return search(matrix, left, mid - 1, target);
-            }
-            return search(matrix, mid + 1, right, target);
+            return ans != -1;
         }
 
         private int get(int[][] matrix, int i) {
@@ -44,11 +44,6 @@ public class SearchA2dMatrix {
             int x = i / n;
             int y = i - x * n;
             return matrix[x][y];
-        }
-
-        private boolean valid(int[][] matrix, int left, int right) {
-            int total = matrix.length * matrix[0].length - 1;
-            return left >= 0 && left <= total && right >= 0 && right <= total;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
