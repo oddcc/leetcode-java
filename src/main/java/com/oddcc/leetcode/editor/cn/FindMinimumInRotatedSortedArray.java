66f6b689058c0e6f75b008706a4d186835b035be
oddcc <skycc71640@gmail.com>
2021-04-08 12:13:27 +0800

    153 每日打卡，循环二分查找

--- src/main/java/com/oddcc/leetcode/editor/cn/FindMinimumInRotatedSortedArray.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/FindMinimumInRotatedSortedArray.java
@@ -15,8 +15,24 @@ public class FindMinimumInRotatedSortedArray {
     class Solution {
         // 每个元素是唯一的，所以可以通过对比子数组的头尾，来判断子数组是有序还是无序
         // 思路1，二分查找，找小于nums[0]的数，当遇到更小的时，更新这个查找的目标，最终一定是找不到，这个目标值就是最小值
+        // 思路2，优化思路1，用循环而不是递归
         public int findMin(int[] nums) {
-            return search(nums, 0, nums.length - 1, nums[0]);
+            int len = nums.length;
+            int left = 0;
+            int right = len - 1;
+            // 当left和right重合时，说明要结束了
+            while (left < right) {
+                int mid = (right - left) / 2 + left;
+                // nums[mid] > nums[right]，说明left~mid是有序的，nums[mid]是区间中的最大值，最小值在mid+1~right中
+                if (nums[mid] > nums[right]) {
+                    left = mid + 1;
+                }
+                // nums[mid] < nums[right]，说明mid~right是有序的，且nums[mid]为区间中最小值，答案可能在left~mid中
+                else {
+                    right = mid;
+                }
+            }
+            return nums[left];
         }
 
         private int search(int[] nums, int left, int right, int target) {
