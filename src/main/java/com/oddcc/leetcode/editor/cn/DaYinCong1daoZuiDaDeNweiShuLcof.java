ecb2076e52c3eb3b31d6ac117a35c3a9174b3663
oddcc <skycc71640@gmail.com>
2021-04-11 11:10:42 +0800

    Offer 17 直接生成

---
+++ src/main/java/com/oddcc/leetcode/editor/cn/DaYinCong1daoZuiDaDeNweiShuLcof.java
@@ -0,0 +1,30 @@
+// 剑指 Offer 17
+
+package com.oddcc.leetcode.editor.cn;
+
+import java.util.Arrays;
+
+public class DaYinCong1daoZuiDaDeNweiShuLcof {
+    public static void main(String[] args) {
+        Solution solution = new DaYinCong1daoZuiDaDeNweiShuLcof().new Solution();
+        System.out.println(Arrays.toString(solution.printNumbers(1)));
+        System.out.println(Arrays.toString(solution.printNumbers(3)));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] printNumbers(int n) {
+            int total = 1;
+            while (n-- > 0) {
+                total *= 10;
+            }
+            int[] ans = new int[total - 1];
+            for (int i = 0; i < ans.length; i++) {
+                ans[i] = i + 1;
+            }
+            return ans;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
