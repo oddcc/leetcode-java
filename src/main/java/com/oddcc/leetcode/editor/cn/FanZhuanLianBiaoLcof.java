9001bdbcffcdd9ff3bc6a48eb48af560a3e06ed4
oddcc <skycc71640@gmail.com>
2021-04-13 15:23:23 +0800

    Offer 24 递归反转链表

--- src/main/java/com/oddcc/leetcode/editor/cn/FanZhuanLianBiaoLcof.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/FanZhuanLianBiaoLcof.java
@@ -8,20 +8,38 @@ public class FanZhuanLianBiaoLcof {
     public static void main(String[] args) {
         Solution solution = new FanZhuanLianBiaoLcof().new Solution();
         System.out.println(solution.reverseList(ListNode.getNodeList(1, 2, 3, 4, 5)));
+        System.out.println(solution.reverseList(ListNode.getNodeList(1)));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         // 思路1，迭代，边遍历边反转链表
+        // 思路2，递归
+
         public ListNode reverseList(ListNode head) {
-            ListNode pre = null;
-            while (head != null) {
-                ListNode t = head.next;
+            if (head == null || head.next == null) return head;
+            ListNode reverseHead = reverse(head.next, head);
+            head.next = null;
+            return reverseHead;
+        }
+
+        /**
+         * 函数的作用是，反转head链表，并返回反转后的链表头
+         * @param head 要反转的链表
+         * @param pre head的前一个节点，可以为null
+         * @return 反转后的链表头
+         */
+        private ListNode reverse(ListNode head, ListNode pre) {
+            // head是链表的最后一个节点，反转，next指向前一个节点，并返回head
+            if (head.next == null) {
                 head.next = pre;
-                pre = head;
-                head = t;
+                return head;
             }
-            return pre;
+            // head不是链表的最后一个节点，把head.next之后的链表反转
+            ListNode reserveHead = reverse(head.next, head);
+            // 反转后，head应该是在反转后的链表尾部，指向pre完成反转
+            head.next = pre;
+            return reserveHead;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
