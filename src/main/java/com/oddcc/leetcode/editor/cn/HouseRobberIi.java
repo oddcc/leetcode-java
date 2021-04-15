be930fed67bbef347134121f12d139eef31dcf6e
oddcc <skycc71640@gmail.com>
2021-04-15 11:34:18 +0800

    213 每日打卡，动态规划

--- src/main/java/com/oddcc/leetcode/editor/cn/HouseRobberIi.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/HouseRobberIi.java
@@ -2,46 +2,43 @@
 
 package com.oddcc.leetcode.editor.cn;
 
+import java.util.Arrays;
+
 public class HouseRobberIi {
     public static void main(String[] args) {
         Solution solution = new HouseRobberIi().new Solution();
-        // System.out.println(solution.rob(new int[]{2, 3, 2}));
-        // System.out.println(solution.rob(new int[]{1, 2, 3, 1}));
-        // System.out.println(solution.rob(new int[]{0}));
+        System.out.println(solution.rob(new int[]{2, 3, 2}));
+        System.out.println(solution.rob(new int[]{1, 2, 3, 1}));
+        System.out.println(solution.rob(new int[]{0}));
         System.out.println(solution.rob(new int[]{114, 117, 207, 117, 235, 82, 90, 67, 143, 146, 53, 108, 200, 91, 80, 223, 58, 170, 110, 236, 81, 90, 222, 160, 165, 195, 187, 199, 114, 235, 197, 187, 69, 129, 64, 214, 228, 78, 188, 67, 205, 94, 205, 169, 241, 202, 144, 240}));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        // 思路1，回溯法找最大值
+        // 思路1，回溯法找最大值，会超时
+        // 思路2，动态规划，dp[i][0]表示没选i的最大值，dp[i][1]表示选了i的最大值
         public int rob(int[] nums) {
-            return maxSum(nums, new boolean[nums.length], 0, 0);
-        }
-
-        /**
-         *
-         * @param nums
-         * @param used 记录抢过哪几家
-         * @param cur 当前位置，还没抢
-         * @param sum 当前的总数，还没抢
-         * @return 返回最大值
-         */
-        private int maxSum(int[] nums, boolean[] used, int cur, int sum) {
-            if (cur >= nums.length) return sum; // 如果越界了，直接返回之前的结果
-            if (cur == nums.length - 1 && used[0]) return sum; // 如果当前是最后一家，而且抢过第一家，那么不能抢最后一家，返回之前的结果
-            // 如果不是第一家，并且上一家抢过了，那么不能抢这一家
-            if (cur - 1 >= 0 && used[cur - 1]) {
-                used[cur] = false;
-                return maxSum(nums, used, cur + 1, sum);
+            int len = nums.length;
+            if (len == 1) return nums[0];
+            int[][] dp = new int[len][2];
+            // 没选第一家的情况
+            dp[0][0] = 0;
+            dp[0][1] = 0;
+            for (int i = 1; i < len; i++) {
+                dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][0]);
+                dp[i][1] = dp[i - 1][0] + nums[i];
             }
-            // 抢了这一家的情况
-            used[cur] = true;
-            int s1 = maxSum(nums, used, cur + 1, sum + nums[cur]);
-            // 没抢这一家的情况
-            used[cur] = false;
-            int s2 = maxSum(nums, used, cur + 1, sum);
-            // 返回结果较大的
-            return Math.max(s1, s2);
+            int ans = Math.max(dp[len - 1][0], dp[len - 1][1]);
+            // 选了第一家的情况
+            for (int[] arr : dp) Arrays.fill(arr, 0);
+            dp[1][0] = nums[0];
+            dp[1][1] = nums[0];
+            for (int i = 2; i < len; i++) {
+                dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][0]);
+                dp[i][1] = dp[i - 1][0] + nums[i];
+            }
+            // 选了第一家的时候，只有不选最后一个，才是答案
+            return Math.max(ans, dp[len - 1][0]);
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
