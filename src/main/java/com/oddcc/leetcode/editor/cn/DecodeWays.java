fdd4c28a17e899b499cf42d685ecef68ac7e495c
oddcc <skycc71640@gmail.com>
2021-04-21 20:25:19 +0800

    91 每日打卡，递归+缓存

--- src/main/java/com/oddcc/leetcode/editor/cn/DecodeWays.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/DecodeWays.java
@@ -2,6 +2,9 @@
 
 package com.oddcc.leetcode.editor.cn;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DecodeWays {
     public static void main(String[] args) {
         Solution solution = new DecodeWays().new Solution();
@@ -13,27 +16,29 @@ public class DecodeWays {
         System.out.println(solution.numDecodings("10"));
         System.out.println(solution.numDecodings("1201234"));
         System.out.println(solution.numDecodings("123123"));
+        System.out.println(solution.numDecodings("230"));
         System.out.println(solution.numDecodings("111111111111111111111111111111111111111111111"));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         // 思路1，递归，超时
+        // 思路2，递归+缓存
         public int numDecodings(String s) {
-            return numDecodings(s.toCharArray(), 0);
+            return numDecodings(s.toCharArray(), 0, new HashMap<>());
         }
 
-        private int numDecodings(char[] chars, int start) {
+        private int numDecodings(char[] chars, int start, Map<Integer, Integer> cache) {
             if (start > chars.length - 1) return 0;
             if (chars[start] == '0') return 0;
             // 剩2个字符时
             if (start == chars.length - 2) {
-                // 如果2开头，且小于等于26，那么选两个是一种方法，分开选是一种方法
-                if (chars[start] == '2' && chars[start + 1] <= '6') return 1 + numDecodings(chars, start + 1);
-                // 如果是1开头，同上，是两种方法
-                if (chars[start] == '1') return 1 + numDecodings(chars, start + 1);
-                // 如果是其他数字开头，只有分开选一种方法
-                return 1;
+                if (!cache.containsKey(start + 1)) cache.put(start + 1, numDecodings(chars, start + 1, cache));
+
+                if ((chars[start] == '2' && chars[start + 1] <= '6') || chars[start] == '1') {
+                    return 1 + cache.get(start + 1);
+                }
+                return cache.get(start + 1);
             }
             // 剩1个字符时
             if (start == chars.length - 1) {
@@ -41,10 +46,14 @@ public class DecodeWays {
                 if (chars[start] == '0') return 0;
                 return 1;
             }
-            // 如果剩多于2个字符，且开始是小于等于26的，或者是1x的，那么都是两种选法
-            if ((chars[start] == '2' && chars[start + 1] <= '6') || chars[start] == '1') return numDecodings(chars, start + 1) + numDecodings(chars, start + 2);
-            // 否则的话，是一种选法
-            return numDecodings(chars, start + 1);
+            if ((chars[start] == '2' && chars[start + 1] <= '6') || chars[start] == '1') {
+                if (!cache.containsKey(start + 1)) cache.put(start + 1, numDecodings(chars, start + 1, cache));
+                if (!cache.containsKey(start + 2)) cache.put(start + 2, numDecodings(chars, start + 2, cache));
+                return cache.get(start + 1) + cache.get(start + 2);
+            }
+
+            if (!cache.containsKey(start + 1)) cache.put(start + 1, numDecodings(chars, start + 1, cache));
+            return cache.get(start + 1);
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
