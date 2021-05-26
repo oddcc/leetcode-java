ef9844bf0682a38aaa1f595645f3a1f396eaf83b
oddcc <skycc71640@gmail.com>
2021-05-26 21:03:51 +0800

    1190 每日打卡，利用栈

--- src/main/java/com/oddcc/leetcode/editor/cn/ReverseSubstringsBetweenEachPairOfParentheses.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/ReverseSubstringsBetweenEachPairOfParentheses.java
@@ -2,6 +2,9 @@
 
 package com.oddcc.leetcode.editor.cn;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class ReverseSubstringsBetweenEachPairOfParentheses {
     public static void main(String[] args) {
         Solution solution = new ReverseSubstringsBetweenEachPairOfParentheses().new Solution();
@@ -11,30 +14,36 @@ public class ReverseSubstringsBetweenEachPairOfParentheses {
         System.out.println(solution.reverseParentheses("a(bcdefghijkl(mno)p)q"));
         System.out.println(solution.reverseParentheses("yfgnxf"));
         System.out.println(solution.reverseParentheses("((eqk((h))))"));
+        System.out.println(solution.reverseParentheses("ta()usw((((a))))"));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
-    // 思路1,递归
+    // 思路1,利用栈
     class Solution {
         public String reverseParentheses(String s) {
-            if (s.startsWith("(") && s.endsWith(")")) {
-                return reverseWithBracket(s);
+            Deque<String> stack = new LinkedList<>();
+            for (Character c : s.toCharArray()) {
+                if (c.equals('(')) {
+                    stack.push("(");
+                }
+                else if (c.equals(')')) {
+                    StringBuilder sb = new StringBuilder();
+                    // 当前是有括号时，栈中应该有一个左括号跟它配对，两个括号之间的内容就是要反转的内容
+                    // 这里append的顺序是出栈的顺序，所以已经是反转的内容
+                    while (stack.peek() != null && !stack.peek().equals("(")) {
+                        sb.append(stack.pop());
+                    }
+                    if (!stack.isEmpty()) stack.pop();
+                    // 我们栈中存的是未反转的内容，所以这里要反转一下
+                    stack.push(reverse(sb.toString()));
+                }
+                else {
+                    stack.push(String.valueOf(c));
+                }
             }
-            return reverseWithOutBracket(s);
-        }
-
-        private String reverseWithOutBracket(String s) {
-            if (!s.contains("(")) return s;
-            int left = s.indexOf("(");
-            int right = s.lastIndexOf(")");
-            String leftSub = s.substring(0, left);
-            String rightSub = s.substring(right + 1);
-            return leftSub + reverseParentheses(s.substring(left + 1, right)) + rightSub;
-        }
-
-        private String reverseWithBracket(String s) {
-            s = s.substring(1, s.length() - 1);
-            return reverse(reverseParentheses(s));
+            StringBuilder ans = new StringBuilder();
+            while (!stack.isEmpty()) ans.append(stack.pop());
+            return reverse(ans.toString());
         }
 
         private String reverse(String s) {
