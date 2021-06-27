6e3afd7026a07a53ed7b6c167e7d28b969484aad
oddcc <skycc71640@gmail.com>
2021-06-27 17:45:56 +0800

    97 优化递归效率,超时

--- src/main/java/com/oddcc/leetcode/editor/cn/InterleavingString.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/InterleavingString.java
@@ -8,6 +8,7 @@ public class InterleavingString {
         System.out.println(solution.isInterleave("aabcc", "dbbca", "aadbbcbcac"));
         System.out.println(solution.isInterleave("aabcc", "dbbca", "aadbbbaccc"));
         System.out.println(solution.isInterleave("", "", ""));
+        System.out.println(solution.isInterleave("ab", "bc", "bcab"));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -15,20 +16,32 @@ public class InterleavingString {
     // 思路1，递归
     class Solution {
         public boolean isInterleave(String s1, String s2, String s3) {
-            if (s1.length() + s2.length() != s3.length()) return false;
-            if (s1.length() == 0) return s2.equals(s3);
-            if (s2.length() == 0) return s1.equals(s3);
+            return isInterleave(s1.toCharArray(), 0, s2.toCharArray(), 0, s3.toCharArray(), 0);
+        }
+
+        private boolean isInterleave(char[] s1, int i1, char[] s2, int i2, char[] s3, int i3) {
+            int l1 = s1.length - i1;
+            int l2 = s2.length - i2;
+            int l3 = s3.length - i3;
+            if (l3 != l1 + l2) return false;
+            if (l2 == 0) return equal(s1, i1, s3, i3);
+            if (l1 == 0) return equal(s2, i2, s3, i3);
             boolean ans = false;
-            // 选了s1的情况
-            if (s1.charAt(0) == s3.charAt(0)) {
-                ans = isInterleave(s1.substring(1), s2, s3.substring(1));
+            if (s1[i1] == s3[i3]) {
+                ans = isInterleave(s1, i1 + 1, s2, i2, s3, i3 + 1);
             }
-            // 选了s2的情况
-            // 两种情况只要有一种满足条件就可以
-            if (s2.charAt(0) == s3.charAt(0)) {
-                ans |= isInterleave(s1, s2.substring(1), s3.substring(1));
+            if (s2[i2] == s3[i3]) {
+                ans |= isInterleave(s1, i1, s2, i2 + 1, s3, i3 + 1);
             }
             return ans;
+        }
+
+        private boolean equal(char[] s1, int i1, char[] s3, int i3) {
+            if (s1.length - i1 != s3.length - i3) return false;
+            for (int i = i1; i < s1.length; i++) {
+                if (s1[i] != s3[i3++]) return false;
+            }
+            return true;
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
