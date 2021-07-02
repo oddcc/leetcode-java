a215c16f0e258f30f8e07ef7d91f6802ff9fbe42
oddcc <skycc71640@gmail.com>
2021-07-02 10:26:53 +0800

    1833 每日打卡，优化内存占用，超时

--- src/main/java/com/oddcc/leetcode/editor/cn/MaximumIceCreamBars.java
+++ src/main/java/com/oddcc/leetcode/editor/cn/MaximumIceCreamBars.java
@@ -2,6 +2,8 @@
 
 package com.oddcc.leetcode.editor.cn;
 
+import java.util.Arrays;
+
 public class MaximumIceCreamBars {
     public static void main(String[] args) {
         Solution solution = new MaximumIceCreamBars().new Solution();
@@ -13,20 +15,23 @@ public class MaximumIceCreamBars {
     //leetcode submit region begin(Prohibit modification and deletion)
     // 背包问题
     // 思路1，动态规划
+    // 思路2，优化空间占用，递推时，只用到二维数组的上一行的结果
     class Solution {
         public int maxIceCream(int[] costs, int coins) {
             // dp[i][j]表示截至到前i个雪糕，能购买的最大数量
             // 针对此题，costs就是要拿的东西，costs[i]就是每样东西的成本，coins可以理解为背包的容量，每拿一样东西的收益都是1
-            int[][] dp = new int[costs.length + 1][coins + 1];
             // dp[0][j]拿0样物品，dp[i][0]拿i样物品放进容量为0的背包，这两种情况收益都是0，不用额外初始化
+            // int[][] dp = new int[costs.length + 1][coins + 1];
+            int[] dp = new int[coins + 1];
             for (int i = 1; i < costs.length + 1; i++) {
+                int[] last = Arrays.copyOf(dp, coins + 1);
                 for (int j = 1; j < coins + 1; j++) {
                     // 对对于第i样物品，对比拿或不拿两种情况
-                    dp[i][j] = Math.max(dp[i - 1][j], j - costs[i - 1] >= 0 ? dp[i - 1][j - costs[i - 1]] + 1 : 0);
+                    dp[j] = Math.max(last[j], j - costs[i - 1] >= 0 ? last[j - costs[i - 1]] + 1 : 0);
                 }
             }
 
-            return dp[costs.length][coins];
+            return dp[coins];
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
